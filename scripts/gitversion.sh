134d8fd62d6735fe4aa80196d44a6d7ea5ff0b26
David Aguilar <davvid@gmail.com>
2008-03-26 04:34:05 -0700

    build: properly detect dirty work tree in gitversion.sh
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- scripts/gitversion.sh
+++ scripts/gitversion.sh
@@ -5,9 +5,9 @@ cd $(dirname $0)
 VN=$(git rev-parse HEAD 2>/dev/null | cut -c1-7)
 LF='
 '
-case "$VERSION" in
+case "$VN" in
 *$LF*) (exit 1) ;;
-v[0-9]*)
+[0-9]*)
 	test -z "$(git diff-index --name-only HEAD)" ||
 		VN="$VN-dirty" ;;
 esac
