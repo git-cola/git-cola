1ba197e3c24a15d6ade528b832333e7cb357ee7c
David Aguilar <davvid@gmail.com>
2008-04-13 18:14:43 -0700

    build: simplify the win32 build script
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- scripts/build-win32.sh
+++ scripts/build-win32.sh
@@ -1,49 +1,15 @@
 #!/bin/sh
+. $(dirname $0)/common.sh
+
 PREFIX=installroot
 BINDIR=$PREFIX/bin
-UGIT=$PREFIX/ugit
-ICONDIR=$PREFIX/share/ugit/icons
-QMDIR=$PREFIX/share/ugit/qm
 DOCDIR=$PREFIX/share/doc/ugit
-cd $(dirname $0)
-cd ..
 
-mkdir -p $BINDIR
-mkdir -p $UGIT
-mkdir -p $ICONDIR
-mkdir -p $QMDIR
+try_python /usr/bin/python2.5 "$PREFIX"
 mkdir -p $DOCDIR
+# no symlinks on win32
+rm -f $BINDIR/bin/ugit
 
-cp README $DOCDIR/README.txt
-cp scripts/ugit-win32.sh $BINDIR
 cp scripts/py2exe-* $BINDIR
-cp ugit/git-ugit $BINDIR
-cp ugit/*.py $UGIT
-cp icons/* $ICONDIR
-
-PYUIC4=$(which pyuic4)
-if [ -z $PYUIC4 ] || [ ! -x $PYUIC4 ]; then
-	echo
-	echo "Could not find pyuic4."
-	echo "You need the pyqt4 developer tools in order to build ugit."
-	echo
-else
-	for file in ui/*.ui; do
-		BASENAME=$(basename $file .ui)
-		$PYUIC4 -x $file -o $UGIT/$BASENAME.py
-	done
-fi
-
-MSGFMT=$(which msgfmt)
-if [ -z $MSGFMT ] || [ ! -x $MSGFMT ]; then
-	echo
-	echo "Could not find msgfmt."
-	echo "You need the msgfmt from GNU gettext in order to create"
-	echo "the translation files for ugit."
-	echo
-else
-	for file in po/*.po; do
-		BASENAME=$(basename $file .po)
-		$MSGFMT --qt -o $QMDIR/$BASENAME.qm $file
-	done
-fi
+cp scripts/ugit-win32.sh $BINDIR
+cp README $DOCDIR/README.txt
--- scripts/mktar-win32.sh
+++ scripts/mktar-win32.sh
@@ -8,7 +8,8 @@ FILE=$BASENAME.tar.gz
 DIR=installroot
 
 if [ -d $DIR ]; then
-	mv $DIR $DIR.old.$$
+	echo "ERROR: '$DIR' already exists."
+	exit -1
 fi
 
 scripts/build-win32.sh
@@ -20,9 +21,6 @@ fi
 rsync -avr $DIR/ $BASENAME/
 tar czf $FILE $BASENAME/
 rm -rf $DIR $BASENAME
-if [ -d $DIR.old.$$ ]; then
-	mv -v $DIR.old.$$ $DIR
-fi
 if [ -e $HOME/htdocs/ugit/releases ]; then
 	mv -v $FILE $HOME/htdocs/ugit/releases
 fi
