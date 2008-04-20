6317264a462bf4602aa156796b5441f28e92106b
David Aguilar <davvid@gmail.com>
2008-04-20 01:09:47 -0700

    release: update version.sh to better handle tags
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,5 +2,5 @@
 VERSION=$(git describe HEAD 2>/dev/null)
 VERSION=$(echo "$VERSION" | sed -e 's/^v//')
 VERSION=$(echo "$VERSION" | sed -e 's/-/./g')
-VERSION=$(echo "$VERSION" | perl -p -e 's/\.[^.]+$//')
+VERSION=$(echo "$VERSION" | perl -p -e 's/(\d+\.\d+)\.[^.]+$/\1/')
 echo $VERSION
