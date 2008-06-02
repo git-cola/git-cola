b3a030dcd1dc96f18344170db19561ce4793e6a3
David Aguilar <davvid@gmail.com>
2008-06-01 23:15:37 -0700

    tests: update git-ops test for GitPython
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- t/t0003-git-ops.py
+++ t/t0003-git-ops.py
@@ -5,17 +5,20 @@ import unittest
 import testutils
 from testutils import pipe
 
-from ugit import git
+from ugit import models
 
 class GitOpsTest(testutils.TestCase):
+
 	def testCommit(self):
 		self.shell("""
 			echo A > A
 			echo B > B
-			git init 2>&1 >/dev/null
+			git init 2>&1 > /dev/null
 			git add A B
 			""")
-		git.commit(m="commit test")
+
+		model = models.Model()
+		model.commit(m="commit test")
 		log = pipe("git log --pretty=oneline | wc -l")
 
 		self.failUnless( '1' == log )
@@ -25,25 +28,26 @@ class GitOpsTest(testutils.TestCase):
 			git init 2>&1 >/dev/null
 			git config section.key value
 		""")
-		value = git.config('section.key', get=True)
+		model = models.Model()
+		value = model.config('section.key', get=True)
 
 		self.failUnless( value == 'value' )
 
 		#  Test config_set
-		git.config_set('section.bool', True)
-		value = git.config('section.bool', get=True)
+		model.config_set('section.bool', True)
+		value = model.config('section.bool', get=True)
 
 		self.failUnless( value == 'true' )
-		git.config_set('section.bool', False)
+		model.config_set('section.bool', False)
 
 		# Test config_dict
-		config_dict = git.config_dict(local=True)
+		config_dict = model.config_dict(local=True)
 
 		self.failUnless( config_dict['section_key'] == 'value' )
 		self.failUnless( config_dict['section_bool'] == False )
 
 		# Test config_dict --global
-		global_dict = git.config_dict(local=False)
+		global_dict = model.config_dict(local=False)
 
 		self.failUnless( type(global_dict) is dict )
 
