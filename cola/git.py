4f9c2d617420bfa537a9e12ad8c4428a3984d94d
David Aguilar <davvid@gmail.com>
2008-07-14 00:39:31 -0700

    cola: include GitPython to make installation simpler for users
    We now ship a copy of GitPython so that users don't have to install
    it themselves.  Another benefit is that we're shielded from API changes.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

---
+++ cola/git.py
@@ -0,0 +1,192 @@
+"""
+This module is from http://gitorious.org/projects/git-python/
+and is included here for convenience.
+"""
+
+import os
+import subprocess
+
+def dashify(string):
+    return string.replace('_', '-')
+
+class MethodMissingMixin(object):
+    """
+    A Mixin' to implement the 'method_missing' Ruby-like protocol.
+
+    This was `taken from a blog post <http://blog.iffy.us/?p=43>`_
+    """
+    def __getattr__(self, attr):
+        class MethodMissing(object):
+            def __init__(self, wrapped, method):
+                self.__wrapped__ = wrapped
+                self.__method__ = method
+            def __call__(self, *args, **kwargs):
+                return self.__wrapped__.method_missing(self.__method__, *args, **kwargs)
+        return MethodMissing(self, attr)
+
+    def method_missing(self, *args, **kwargs):
+        """ This method should be overridden in the derived class. """
+        raise NotImplementedError(str(self.__wrapped__) + " 'method_missing' method has not been implemented.")
+
+
+# Enables debugging of GitPython's git commands
+GIT_PYTHON_TRACE = os.environ.get("GIT_PYTHON_TRACE", False)
+
+execute_kwargs = ('istream', 'with_keep_cwd', 'with_extended_output',
+                  'with_exceptions', 'with_raw_output')
+
+class Git(MethodMissingMixin):
+    """
+    The Git class manages communication with the Git binary
+    """
+    def __init__(self, git_dir):
+        super(Git, self).__init__()
+        self.git_dir = git_dir
+
+    @property
+    def get_dir(self):
+        return self.git_dir
+
+    def execute(self, command,
+                istream=None,
+                with_keep_cwd=False,
+                with_extended_output=False,
+                with_exceptions=True,
+                with_raw_output=False,
+                ):
+        """
+        Handles executing the command on the shell and consumes and returns
+        the returned information (stdout)
+
+        ``command``
+            The command argument list to execute
+
+        ``istream``
+            Standard input filehandle passed to subprocess.Popen.
+
+        ``with_keep_cwd``
+            Whether to use the current working directory from os.getcwd().
+            GitPython uses get_work_tree() as its working directory by
+            default and get_git_dir() for bare repositories.
+
+        ``with_extended_output``
+            Whether to return a (status, stdout, stderr) tuple.
+
+        ``with_exceptions``
+            Whether to raise an exception when git returns a non-zero status.
+
+        ``with_raw_output``
+            Whether to avoid stripping off trailing whitespace.
+
+        Returns
+            str(output)                     # extended_output = False (Default)
+            tuple(int(status), str(output)) # extended_output = True
+        """
+
+        if GIT_PYTHON_TRACE and not GIT_PYTHON_TRACE == 'full':
+            print ' '.join(command)
+
+        # Allow the user to have the command executed in their working dir.
+        if with_keep_cwd or self.git_dir is None:
+          cwd = os.getcwd()
+        else:
+          cwd=self.git_dir
+
+        # Start the process
+        proc = subprocess.Popen(command,
+                                cwd=cwd,
+                                stdin=istream,
+                                stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE
+                                )
+
+        # Wait for the process to return
+        try:
+            stdout_value = proc.stdout.read()
+            stderr_value = proc.stderr.read()
+            status = proc.wait()
+        finally:
+            proc.stdout.close()
+            proc.stderr.close()
+
+        # Strip off trailing whitespace by default
+        if not with_raw_output:
+            stdout_value = stdout_value.rstrip()
+            stderr_value = stderr_value.rstrip()
+
+        if with_exceptions and status != 0:
+            raise GitCommandError(command, status, stderr_value)
+
+        if GIT_PYTHON_TRACE == 'full':
+            if stderr_value:
+              print "%s -> %d: '%s' !! '%s'" % (command, status, stdout_value, stderr_value)
+            elif stdout_value:
+              print "%s -> %d: '%s'" % (command, status, stdout_value)
+            else:
+              print "%s -> %d" % (command, status)
+
+        # Allow access to the command's status code
+        if with_extended_output:
+            return (status, stdout_value, stderr_value)
+        else:
+            return stdout_value
+
+    def transform_kwargs(self, **kwargs):
+        """
+        Transforms Python style kwargs into git command line options.
+        """
+        args = []
+        for k, v in kwargs.items():
+            if len(k) == 1:
+                if v is True:
+                    args.append("-%s" % k)
+                elif type(v) is not bool:
+                    args.append("-%s%s" % (k, v))
+            else:
+                if v is True:
+                    args.append("--%s" % dashify(k))
+                elif type(v) is not bool:
+                    args.append("--%s=%s" % (dashify(k), v))
+        return args
+
+    def method_missing(self, method, *args, **kwargs):
+        """
+        Run the given git command with the specified arguments and return
+        the result as a String
+
+        ``method``
+            is the command
+
+        ``args``
+            is the list of arguments
+
+        ``kwargs``
+            is a dict of keyword arguments.
+            This function accepts the same optional keyword arguments
+            as execute().
+
+        Examples
+            git.rev_list('master', max_count=10, header=True)
+
+        Returns
+            Same as execute()
+        """
+
+        # Handle optional arguments prior to calling transform_kwargs
+        # otherwise these'll end up in args, which is bad.
+        _kwargs = {}
+        for kwarg in execute_kwargs:
+            try:
+                _kwargs[kwarg] = kwargs.pop(kwarg)
+            except KeyError:
+                pass
+
+        # Prepare the argument list
+        opt_args = self.transform_kwargs(**kwargs)
+        ext_args = map(str, args)
+        args = opt_args + ext_args
+
+        call = ["git", dashify(method)]
+        call.extend(args)
+
+        return self.execute(call, **_kwargs)
