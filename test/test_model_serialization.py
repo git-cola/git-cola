73ecfad6264d33abdb59bf5e9e71fc885abada54
David Aguilar <davvid@gmail.com>
2009-01-16 21:05:13 -0800

    tests: skip serialization tests when simplejson is not available
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/test_model_serialization.py
+++ test/test_model_serialization.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 import os
+import sys
 import unittest
 
 import testlib
 from testlib import InnerModel
 from testlib import NestedModel
 
+import cola.model
 from cola.model import Model
 
 class TestSaveRestore(unittest.TestCase):
     def setUp(self):
-        testlib.create_test_dir()
-
-        self.nested = NestedModel()
-        path = os.path.join(testlib.get_test_dir(), 'test.data')
-        # save & reconstitute
-        self.nested.save(path)
-        self.clone = Model.instance(path)
+        self._has_json = cola.model.has_json()
+        if self._has_json:
+            testlib.create_test_dir()
+            self.nested = NestedModel()
+            path = os.path.join(testlib.get_test_dir(), 'test.data')
+            # save & reconstitute
+            self.nested.save(path)
+            self.clone = Model.instance(path)
 
     def tearDown(self):
-        testdir = testlib.get_test_dir()
-        if os.path.exists(testdir):
-            testlib.remove_test_dir()
+        if self._has_json:
+            testdir = testlib.get_test_dir()
+            if os.path.exists(testdir):
+                testlib.remove_test_dir()
 
     def testCloneToClass(self):
         self.failUnless( str(NestedModel) ==
@@ -54,6 +58,14 @@ class TestSaveRestore(unittest.TestCase):
     def testObjectInDictInList(self):
         self.failUnless( str(InnerModel) ==
                          str(self.clone.innerList[-1]["foo"].__class__))
+
+# All of these tests require simplejson.
+# Replace all test methods with a stub lambda function
+# if simplejson is not available.
+if not cola.model.has_json():
+    for thing in dir(TestSaveRestore):
+        if thing.startswith('test'):
+            setattr(TestSaveRestore, thing, lambda x: True)
 
 if __name__ == '__main__':
     unittest.main()
