35df8357b4245d0384c71307f264d39b324e46fa
David Aguilar <davvid@gmail.com>
2009-02-07 19:04:37 -0800

    tests: lesson the buffer sizes in the test_git tests
    We probably only need to be bigger than PAGE_SIZE so to catch
    deadlock so set our buffers to PAGE_SIZE * 2
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/test_git.py
+++ test/test_git.py
@@ -24,15 +24,15 @@ class GitCommandTest(unittest.TestCase):
         self.failUnless(contents[0] == '/build')
 
     def test_stdout(self):
-        status, out = git.Git.execute([helper.fixture('stdout.py'), '65536'],
+        status, out = git.Git.execute([helper.fixture('stdout.py'), '8192'],
                                       with_extended_output=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
-        self.assertEqual(len(out), 65536)
+        self.assertEqual(len(out), 8192)
 
     def test_stderr_empty(self):
         """Test that stderr is ignored by Git.execute() without the with_stderr option."""
-        status, out = git.Git.execute([helper.fixture('stderr.py'), '65536'],
+        status, out = git.Git.execute([helper.fixture('stderr.py'), '8192'],
                                       with_extended_output=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
@@ -40,58 +40,58 @@ class GitCommandTest(unittest.TestCase):
 
     def test_stderr_nonempty_with_stderr(self):
         """Test that with_stderr makes Git.execute() see stderr."""
-        status, out = git.Git.execute([helper.fixture('stderr.py'), '65536'],
+        status, out = git.Git.execute([helper.fixture('stderr.py'), '8192'],
                                       with_extended_output=True,
                                       with_stderr=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
-        self.assertEqual(len(out), 65536)
+        self.assertEqual(len(out), 8192)
 
     def test_stdout_and_stderr_ignores_stderr(self):
         """Test ignoring stderr when both are provided."""
         status, out = git.Git.execute([helper.fixture('stdout_and_stderr.py'),
-                                       '65536',
+                                       '8192',
                                        'stdout'], # otherwise, same as below
                                       with_extended_output=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
-        self.assertEqual(len(out), 65536)
+        self.assertEqual(len(out), 8192)
 
     def test_stdout_and_stderr_ignores_stderr_v2(self):
         """Test ignoring stderr when both are provided. (v2)"""
         # stdout_and_stderr.py swaps the order of the stdout, stderr write()
         # calls when argv[1] == 'stderr'.
         status, out = git.Git.execute([helper.fixture('stdout_and_stderr.py'),
-                                       '65536',
+                                       '8192',
                                        'stderr'], # otherwise, same as above
                                       with_extended_output=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
-        self.assertEqual(len(out), 65536)
+        self.assertEqual(len(out), 8192)
 
     def test_stdout_and_stderr_sees_stderr(self):
         """Test seeing both stderr and stdout when both are available."""
         status, out = git.Git.execute([helper.fixture('stdout_and_stderr.py'),
-                                       '65536',
+                                       '8192',
                                        'stdout'], # otherwise, same as below
                                       with_extended_output=True,
                                       with_stderr=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
-        self.assertEqual(len(out), 65536 * 2)
+        self.assertEqual(len(out), 8192 * 2)
 
     def test_stdout_and_stderr_sees_stderr_v2(self):
         """Test seeing both stderr and stdout when both are available (v2)."""
         # stdout_and_stderr.py swaps the order of the stdout, stderr write()
         # calls when argv[1] == 'stderr'.
         status, out = git.Git.execute([helper.fixture('stdout_and_stderr.py'),
-                                       '65536',
+                                       '8192',
                                        'stderr'], # otherwise, same as above
                                       with_extended_output=True,
                                       with_stderr=True,
                                       with_raw_output=True)
         self.assertEqual(status, 0)
-        self.assertEqual(len(out), 65536 * 2)
+        self.assertEqual(len(out), 8192 * 2)
 
 if __name__ == '__main__':
     unittest.main()
