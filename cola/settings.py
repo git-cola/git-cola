63a317d948ee7448d13df723dfafac333a1c9d4b
David Aguilar <davvid@gmail.com>
2009-02-07 19:09:00 -0800

    settings: remove the import checks in the settings module
    Now that json is built-in we don't have to check whether or not we
    have them available.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/settings.py
+++ cola/settings.py
@@ -3,30 +3,20 @@
 """This handles saving complex settings such as bookmarks, etc.
 """
 
-HAS_SIMPLEJSON = False
-try:
-    import simplejson
-    HAS_SIMPLEJSON = True
-except ImportError:
-    pass
-
 import os
 import user
 from cola.model import Model
 
 class SettingsModel(Model):
-    def init(self):
-        self.create( bookmarks = [] )
-        if not HAS_SIMPLEJSON:
-            return
+    def __init__(self):
+        Model.__init__(self)
+        self.bookmarks = []
         settings = self.path()
         if os.path.exists(settings):
             self.load(settings)
 
     def path(self):
         return os.path.join(user.home, '.cola')
-    
+
     def save_all_settings(self):
-        if not HAS_SIMPLEJSON:
-            return
         self.save(self.path())
