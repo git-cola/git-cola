ee3fab5ce21fd35855f0a2aaf1f532b5028e4b40
David Aguilar <davvid@gmail.com>
2009-04-17 03:46:47 -0700

    resources: add more source comments
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/resources.py
+++ cola/resources.py
@@ -5,9 +5,11 @@ from os.path import join
 
 def path(*args):
     if 'site-packages' in __file__:
-        # lib/python2.x/site-packages/cola/__file__.py
+        # this is the release tree
+        # __file__ = '$prefix/lib/python2.x/site-packages/cola/__file__.py'
         prefix = dirname(dirname(dirname(dirname(dirname(abspath(__file__))))))
     else:
-        # this is the source tree <src>/cola/__file__.py
+        # this is the source tree
+        # __file__ = '$prefix/cola/__file__.py'
         prefix = dirname(dirname(abspath(__file__)))
     return join(prefix, *args)
