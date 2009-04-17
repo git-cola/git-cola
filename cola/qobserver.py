32cf8cefb08554da3e1b705fc8818e76675ab34c
David Aguilar <davvid@gmail.com>
2009-04-17 03:49:05 -0700

    qobserver: minimize imports and avoid double-underscores
    This lessens namespace pollution and makes the code more obvious.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/qobserver.py
+++ cola/qobserver.py
@@ -10,73 +10,54 @@ are always available in the model without having to worry about the
 different ways to query Qt widgets.
 
 """
+from PyQt4 import QtCore
+from PyQt4 import QtGui
 
+from cola import observer
 
-from PyQt4.QtCore import Qt
-from PyQt4.QtCore import QObject
-from PyQt4.QtCore import SIGNAL
-from PyQt4.QtCore import QDate
-from PyQt4.QtGui import QComboBox
-from PyQt4.QtGui import QDateEdit
-from PyQt4.QtGui import QSpinBox
-from PyQt4.QtGui import QPixmap
-from PyQt4.QtGui import QTextEdit
-from PyQt4.QtGui import QLineEdit
-from PyQt4.QtGui import QListWidget
-from PyQt4.QtGui import QCheckBox
-from PyQt4.QtGui import QFontComboBox
-from PyQt4.QtGui import QAbstractButton
-from PyQt4.QtGui import QSplitter
-from PyQt4.QtGui import QAction
-from PyQt4.QtGui import QTreeWidget
-from PyQt4.QtGui import QTreeWidgetItem
-from PyQt4.QtCore import QVariant
-
-from cola.observer import Observer
-
-
-class QObserver(Observer, QObject):
+class QObserver(observer.Observer, QtCore.QObject):
 
     def __init__(self, model, view, *args, **kwargs):
-        Observer.__init__(self, model)
-        QObject.__init__(self)
+        """Binds a model and Qt view"""
+        observer.Observer.__init__(self, model)
+        QtCore.QObject.__init__(self)
 
         self.view = view
 
-        self.__actions = {}
-        self.__callbacks = {}
-        self.__model_to_view = {}
-        self.__view_to_model = {}
-        self.__connected = set()
-        self.__in_textfield = False
-        self.__in_callback = False
+        self._actions = {}
+        self._callbacks = {}
+        self._model_to_view = {}
+        self._view_to_model = {}
+        self._connected = set()
+        self._in_textfield = False
+        self._in_callback = False
 
     def SLOT(self, *args):
         """Default slot to handle all Qt callbacks.
         This method delegates to callbacks from add_signals."""
 
-        self.__in_textfield = False
+        self._in_textfield = False
 
         widget = self.sender()
         sender = str(widget.objectName())
 
-        if sender in self.__view_to_model:
+        if sender in self._view_to_model:
             model = self.model
-            model_param = self.__view_to_model[sender]
-            if isinstance(widget, QTextEdit):
-                self.__in_textfield = True
+            model_param = self._view_to_model[sender]
+            if isinstance(widget, QtGui.QTextEdit):
+                self._in_textfield = True
                 value = unicode(widget.toPlainText())
                 model.set_param(model_param, value,
                                 notify=False)
-            elif isinstance(widget, QLineEdit):
-                self.__in_textfield = True
+            elif isinstance(widget, QtGui.QLineEdit):
+                self._in_textfield = True
                 value = unicode(widget.text())
                 model.set_param(model_param, value)
-            elif isinstance(widget, QCheckBox):
+            elif isinstance(widget, QtGui.QCheckBox):
                 model.set_param(model_param, widget.isChecked())
-            elif isinstance(widget, QSpinBox):
+            elif isinstance(widget, QtGui.QSpinBox):
                 model.set_param(model_param, widget.value())
-            elif isinstance(widget, QFontComboBox):
+            elif isinstance(widget, QtGui.QFontComboBox):
                 value = unicode(widget.currentFont().toString())
                 if model.has_param(model_param+'_size'):
                     size = model.get_param(model_param+'_size')
@@ -84,11 +65,11 @@ class QObserver(Observer, QObject):
                     props[1] = str(size)
                     value = ','.join(props)
                 model.set_param(model_param, value)
-            elif isinstance(widget, QDateEdit):
-                fmt = Qt.ISODate
+            elif isinstance(widget, QtGui.QDateEdit):
+                fmt = QtCore.Qt.ISODate
                 value = str(widget.date().toString(fmt))
                 model.set_param(model_param, value)
-            elif isinstance(widget, QListWidget):
+            elif isinstance(widget, QtGui.QListWidget):
                 row = widget.currentRow()
                 item = widget.item(row)
                 if item:
@@ -102,7 +83,7 @@ class QObserver(Observer, QObject):
                                     model.get_param(model_param)[row])
                 else:
                     model.set_param(model_param+'_item', '')
-            elif isinstance(widget, QTreeWidget):
+            elif isinstance(widget, QtGui.QTreeWidget):
                 item = widget.currentItem()
                 if item:
                     selected = item.isSelected()
@@ -117,7 +98,7 @@ class QObserver(Observer, QObject):
                     model.set_param(model_param+'_item', items[row])
                 else:
                     model.set_param(model_param+'_item', '')
-            elif isinstance(widget, QComboBox):
+            elif isinstance(widget, QtGui.QComboBox):
                 idx = widget.currentIndex()
                 model.set_param(model_param+'_index', idx)
                 if idx != -1 and model.get_param(model_param):
@@ -129,19 +110,19 @@ class QObserver(Observer, QObject):
             else:
                 print("SLOT(): Unknown widget:", sender, widget)
 
-        self.__in_callback = True
-        if sender in self.__callbacks:
-            self.__callbacks[sender](*args)
-        self.__in_callback = False
-        self.__in_textfield = False
+        self._in_callback = True
+        if sender in self._callbacks:
+            self._callbacks[sender](*args)
+        self._in_callback = False
+        self._in_textfield = False
 
     def connect(self, obj, signal_str, *args):
         """Convenience function so that subclasses do not have
         to import QtCore.SIGNAL."""
         signal = signal_str
         if type(signal) is str:
-            signal = SIGNAL(signal)
-        return QObject.connect(obj, signal, *args)
+            signal = QtCore.SIGNAL(signal)
+        return QtCore.QObject.connect(obj, signal, *args)
 
     def add_signals(self, signal_str, *objects):
         """Connects object's signal to the QObserver."""
@@ -151,7 +132,7 @@ class QObserver(Observer, QObject):
     def add_callbacks(self, **callbacks):
         """Registers callbacks that are called in response to GUI events."""
         for sender, callback in callbacks.iteritems():
-            self.__callbacks[sender] = callback
+            self._callbacks[sender] = callback
             self.autoconnect(getattr(self.view, sender))
 
     def add_observables(self, *params):
@@ -162,35 +143,35 @@ class QObserver(Observer, QObject):
 
     def model_to_view(self, model_param, *widgets):
         """Binds model params to qt widgets(model->view)"""
-        self.__model_to_view[model_param] = widgets
+        self._model_to_view[model_param] = widgets
         for w in widgets:
             view = str(w.objectName())
-            self.__view_to_model[view] = model_param
-            self.__autoconnect(w)
+            self._view_to_model[view] = model_param
+            self._autoconnect(w)
 
     def autoconnect(self, *widgets):
         for w in widgets:
-            self.__autoconnect(w)
+            self._autoconnect(w)
 
-    def __autoconnect(self, widget):
+    def _autoconnect(self, widget):
         """Automagically connects Qt widgets to QObserver.SLOT"""
 
-        if widget in self.__connected:
+        if widget in self._connected:
             return
-        self.__connected.add(widget)
+        self._connected.add(widget)
 
-        if isinstance(widget, QTextEdit):
+        if isinstance(widget, QtGui.QTextEdit):
             self.add_signals('textChanged()', widget)
-        elif isinstance(widget, QLineEdit):
+        elif isinstance(widget, QtGui.QLineEdit):
             self.add_signals('textChanged(const QString&)', widget)
-        elif isinstance(widget, QListWidget):
+        elif isinstance(widget, QtGui.QListWidget):
             self.add_signals('itemSelectionChanged()', widget)
             self.add_signals('itemClicked(QListWidgetItem *)', widget)
             doubleclick = str(widget.objectName())+'_doubleclick'
             if hasattr(self, doubleclick):
                 self.connect(widget, 'itemDoubleClicked(QListWidgetItem *)',
                              getattr(self, doubleclick))
-        elif isinstance(widget, QTreeWidget):
+        elif isinstance(widget, QtGui.QTreeWidget):
             self.add_signals('itemSelectionChanged()', widget)
             self.add_signals('itemClicked(QTreeWidgetItem *, int)', widget)
             doubleclick = str(widget.objectName())+'_doubleclick'
@@ -198,21 +179,21 @@ class QObserver(Observer, QObject):
                 self.connect(widget, 'itemDoubleClicked(QTreeWidgetItem *, int)',
                              getattr(self, doubleclick))
 
-        elif isinstance(widget, QAbstractButton):
+        elif isinstance(widget, QtGui.QAbstractButton):
             self.add_signals('released()', widget)
-        elif isinstance(widget, QAction):
+        elif isinstance(widget, QtGui.QAction):
             self.add_signals('triggered()', widget)
-        elif isinstance(widget, QCheckBox):
+        elif isinstance(widget, QtGui.QCheckBox):
             self.add_signals('stateChanged(int)', widget)
-        elif isinstance(widget, QSpinBox):
+        elif isinstance(widget, QtGui.QSpinBox):
             self.add_signals('valueChanged(int)', widget)
-        elif isinstance(widget, QFontComboBox):
+        elif isinstance(widget, QtGui.QFontComboBox):
             self.add_signals('currentFontChanged(const QFont&)', widget)
-        elif isinstance(widget, QSplitter):
+        elif isinstance(widget, QtGui.QSplitter):
             self.add_signals('splitterMoved(int,int)', widget)
-        elif isinstance(widget, QDateEdit):
+        elif isinstance(widget, QtGui.QDateEdit):
             self.add_signals('dateChanged(const QDate&)', widget)
-        elif isinstance(widget, QComboBox):
+        elif isinstance(widget, QtGui.QComboBox):
             self.add_signals('currentIndexChanged(int)', widget)
         else:
             raise Exception('Asked to connect unknown widget:\n\t%s => %s'
@@ -223,33 +204,33 @@ class QObserver(Observer, QObject):
         view changes.(view->model)"""
         for model_param, callback in kwargs.iteritems():
             if type(callback) is list:
-                self.__actions[model_param] = callback
+                self._actions[model_param] = callback
             else:
-                self.__actions[model_param] = [callback]
+                self._actions[model_param] = [callback]
 
     def subject_changed(self, param, value):
         """Sends a model param to the view(model->view)"""
 
-        if self.__in_textfield and not self.__in_callback:
+        if self._in_textfield and not self._in_callback:
             # A slot has changed the model and we're not in
             # a user callback.  In this case the event is causing
             # a feedback loop so skip redundant work and return.
             return
 
-        if param in self.__model_to_view:
+        if param in self._model_to_view:
             notify = self.model.get_notify()
             self.model.set_notify(False)
-            for widget in self.__model_to_view[param]:
+            for widget in self._model_to_view[param]:
                 sender = str(widget.objectName())
-                if isinstance(widget, QSpinBox):
+                if isinstance(widget, QtGui.QSpinBox):
                     widget.setValue(value)
-                elif isinstance(widget, QPixmap):
+                elif isinstance(widget, QtGui.QPixmap):
                     widget.load(value)
-                elif isinstance(widget, QTextEdit):
+                elif isinstance(widget, QtGui.QTextEdit):
                     widget.setText(value)
-                elif isinstance(widget, QLineEdit):
+                elif isinstance(widget, QtGui.QLineEdit):
                     widget.setText(value)
-                elif isinstance(widget, QListWidget):
+                elif isinstance(widget, QtGui.QListWidget):
                     self.model.set_param(param+'_item', '')
                     widget.clear()
                     for i in value:
@@ -261,19 +242,20 @@ class QObserver(Observer, QObject):
                             widget.setCurrentItem(item)
                             widget.setItemSelected(item, True)
                             self.model.set_param(param+'_item', value[idx])
-                            if sender in self.__callbacks:
+                            if sender in self._callbacks:
                                 self.model.set_notify(True)
-                                self.__callbacks[sender]()
+                                self._callbacks[sender]()
                                 self.model.set_notify(False)
                         else:
                             self.model.set_param(param+'_item', '')
-                elif isinstance(widget, QTreeWidget):
+                elif isinstance(widget, QtGui.QTreeWidget):
                     self.model.set_param(param+'_item', '')
                     widget.clear()
                     for i in value:
-                        item = QTreeWidgetItem([i])
-                        item.setData(0, Qt.UserRole,
-                                     QVariant(widget.topLevelItemCount()))
+                        item = QtGui.QTreeWidgetItem([i])
+                        count = widget.topLevelItemCount()
+                        item.setData(0, QtCore.Qt.UserRole,
+                                     QtCore.QVariant(count))
                         widget.addTopLevelItem(item)
                     if self.model.has_param(param+'_index'):
                         idx = self.model.get_param(param+'_index')
@@ -283,26 +265,26 @@ class QObserver(Observer, QObject):
                             widget.setItemSelected(item, True)
                             val = value[idx]
                             self.model.set_param(param+'_item', val)
-                            if sender in self.__callbacks:
+                            if sender in self._callbacks:
                                 self.model.set_notify(True)
-                                self.__callbacks[sender]()
+                                self._callbacks[sender]()
                                 self.model.set_notify(False)
                         else:
                             self.model.set_param(param+'_item', '')
 
-                elif isinstance(widget, QCheckBox):
+                elif isinstance(widget, QtGui.QCheckBox):
                     widget.setChecked(value)
-                elif isinstance(widget, QFontComboBox):
+                elif isinstance(widget, QtGui.QFontComboBox):
                     font = widget.currentFont()
                     font.fromString(value)
-                elif isinstance(widget, QDateEdit):
+                elif isinstance(widget, QtGui.QDateEdit):
                     if not value:
                         return
-                    fmt = Qt.ISODate
-                    date = QDate.fromString(value, fmt)
+                    fmt = QtCore.Qt.ISODate
+                    date = QtCore.QDate.fromString(value, fmt)
                     if date:
                         widget.setDate(date)
-                elif isinstance(widget, QComboBox):
+                elif isinstance(widget, QtCore.QComboBox):
                     self.model.set_param(param+'_item', '')
                     widget.clear()
                     for item in value:
@@ -317,21 +299,21 @@ class QObserver(Observer, QObject):
                           str(widget.objectName()), widget, value)
             self.model.set_notify(notify)
 
-        if param not in self.__actions:
+        if param not in self._actions:
             return
         widgets = []
-        if param in self.__model_to_view:
-            for widget in self.__model_to_view[param]:
+        if param in self._model_to_view:
+            for widget in self._model_to_view[param]:
                 widgets.append(widget)
         # Call the model callback w/ the view's widgets as the args
-        for action in self.__actions[param]:
+        for action in self._actions[param]:
             action(*widgets)
 
     def refresh_view(self, *params):
         """Sends a notification message for each known model parameter."""
         if not params:
-            params= tuple(self.__model_to_view.keys()
-                         +self.__actions.keys())
+            params= tuple(self._model_to_view.keys() +
+                          self._actions.keys())
         notified = []
         for param in params:
             if param not in notified:
