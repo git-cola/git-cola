52faaf1f00a54c822bbc297c3e04db45f248f3e9
David Aguilar <davvid@gmail.com>
2009-04-19 21:41:54 -0700

    app: make ColaApplication test-friendly by using less derivation
    ColaApplication no longer inherits from QApplication.  It now
    contains either a QApplication or QCoreApplication based on the value
    of the gui=<bool> construction option.
    
    Consequently, this makes tests work without a gui =)
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/app.py
+++ cola/app.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009, David Aguilar <davvid@gmail.com>
 """Provides the cola QApplication subclass"""
+# style note: we use camelCase here since we're masquerading a Qt class
 
 import os
 
@@ -8,35 +9,49 @@ from PyQt4 import QtGui
 
 from cola import utils
 
+class ColaApplication(object):
+    """The main cola application
 
-class ColaApplication(QtGui.QApplication):
-    """This makes translation work by throwing away the context
+    ColaApplication handles i18n of user-visible data
     """
 
-    def __init__(self, argv, locale=None):
+    def __init__(self, argv, locale=None, gui=True):
         """Initialize our QApplication for translation
         """
-        QtGui.QApplication.__init__(self, argv)
+        # monkey-patch Qt's app translate() to handle .po files
+        if gui:
+            self._app = QtGui.QApplication(argv)
+            self._app.setWindowIcon(QtGui.QIcon(utils.get_icon('git.png')))
+            self._translate_base = QtGui.QApplication.translate
+            QtGui.QApplication.translate = self.translate
+        else:
+            self._app = QtCore.QCoreApplication(argv)
+            self._translate_base = QtCore.QCoreApplication.translate
+            QtCore.QCoreApplication.translate = self.translate
 
-        # Handle i18n -- load translation files and install a translator
-        # We do this by monkey-patching QApplication.translate
-        self._translate = QtGui.QApplication.translate
-        QtGui.QApplication.translate = self.translate
+        # Find the current language settings and apply them
         if not locale:
             locale = str(QtCore.QLocale().system().name())
+
         qmfile = utils.get_qm_for_locale(locale)
         if os.path.exists(qmfile):
-            translator = QtCore.QTranslator(self)
+            translator = QtCore.QTranslator(self._app)
             translator.load(qmfile)
-            self.installTranslator(translator)
-
-        self.setWindowIcon(QtGui.QIcon(utils.get_icon('git.png')))
+            self._app.installTranslator(translator)
 
     def translate(self, context, *args):
         """Supports @@noun/@@verb and context-less translation
         """
         # We set the context to '' to properly handle .qm files
-        trtxt = unicode(self._translate('', *args))
+        trtxt = unicode(self._translate_base('', *args))
         if trtxt[-6:-4] == '@@': # handle @@verb / @@noun
             trtxt = trtxt[:-6]
         return trtxt
+
+    def activeWindow(self):
+        """Wrapper around our QApp.activeWindow()"""
+        return self._app.activeWindow()
+
+    def exec_(self):
+        """Wrapper around our QApp.exec_()"""
+        return self._app.exec_()
--- test/test_cola_app.py
+++ test/test_cola_app.py
@@ -1,10 +1,7 @@
 # -*- encoding: utf-8 -*-
-import os
-import sys
 import unittest
 
-from cola.app import ColaApplication
-
+import cola.app
 
 class ColaApplicationTestCase(unittest.TestCase):
     """Test cases for the ColaApplication class"""
@@ -12,31 +9,31 @@ class ColaApplicationTestCase(unittest.TestCase):
     def test_translates_noun(self):
         """Test that strings with @@noun are translated
         """
-        app = ColaApplication([], locale='ja_JP')
+        app = cola.app.ColaApplication([], locale='ja_JP', gui=False)
         self.assertEqual(app.translate('??', 'Commit@@noun'), u'コミット')
 
     def test_translates_verb(self):
         """Test that strings with @@verb are translated
         """
-        app = ColaApplication([], locale='de_DE')
+        app = cola.app.ColaApplication([], locale='de_DE', gui=False)
         self.assertEqual(app.translate('??', 'Commit@@verb'), 'Eintragen')
 
     def test_translates_english_noun(self):
         """Test that English strings with @@noun are properly handled
         """
-        app = ColaApplication([], locale='en_US.UTF-8')
+        app = cola.app.ColaApplication([], locale='en_US.UTF-8', gui=False)
         self.assertEqual(app.translate('??', 'Commit@@noun'), 'Commit')
 
     def test_translates_english_verb(self):
         """Test that English strings with @@verb are properly handled
         """
-        app = ColaApplication([], locale='en_US.UTF-8')
+        app = cola.app.ColaApplication([], locale='en_US.UTF-8', gui=False)
         self.assertEqual(app.translate('??', 'Commit@@verb'), 'Commit')
 
     def test_translates_random_english(self):
         """Test that random English strings are passed through as-is
         """
-        app = ColaApplication([], locale='en_US.UTF-8')
+        app = cola.app.ColaApplication([], locale='en_US.UTF-8', gui=False)
         self.assertEqual(app.translate('??', 'Random'), 'Random')
 
 
