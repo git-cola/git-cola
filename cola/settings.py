e524af1577d09a9f0bab44dbd7ce556d08be87ae
David Aguilar <davvid@gmail.com>
2009-04-24 19:20:17 -0700

    settings: add a SettingsManager singleton
    The SettingsManager class provides an interface for view
    de/serialization.  We'll use this to handle saving gui settings.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/settings.py
+++ cola/settings.py
@@ -14,6 +14,7 @@ class SettingsModel(Model, observable.Observable):
         Model.__init__(self)
         observable.Observable.__init__(self)
         self.bookmarks = []
+        self.gui_state = {}
         self.load()
 
     def path(self):
@@ -40,3 +41,45 @@ class SettingsModel(Model, observable.Observable):
         # Restore properties
         self.set_notify(notify)
         self.set_observers(observers)
+
+    def set_gui_state(self, name, state):
+        """Sets GUI state for a view"""
+        self.gui_state[name] = state
+
+    def get_gui_state(self, name):
+        """Returns the state an gui"""
+        return self.gui_state.get(name, {})
+
+    def add_bookmark(self, bookmark):
+        """Adds a bookmark to the saved settings"""
+        if bookmark not in self.bookmarks:
+            self.bookmarks.append(bookmark)
+
+    def remove_bookmark(self, bookmark):
+        """Removes a bookmark from the saved settings"""
+        if bookmark in self.bookmarks:
+            self.bookmarks.remove(bookmark)
+
+class SettingsManager(object):
+    """Manages a SettingsModel singleton
+    """
+    _settings = SettingsModel()
+
+    @staticmethod
+    def settings():
+        """Returns the SettingsModel singleton"""
+        return SettingsManager._settings
+
+    @staticmethod
+    def save_gui_state(gui):
+        """Saves settings for a cola view"""
+        name = gui.name()
+        state = gui.export_state()
+        model = SettingsManager.settings()
+        model.set_gui_state(name, state)
+        model.save()
+
+    @staticmethod
+    def get_gui_state(gui):
+        """Returns the state for a gui"""
+        return SettingsManager.settings().get_gui_state(gui.name())
