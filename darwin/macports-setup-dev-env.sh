cb1237ce10738685045ab29b25eb28331841da8c
David Aguilar <davvid@gmail.com>
2009-05-10 15:47:31 -0700

    darwin: change setup-dev-env script to list the minimal dependencies
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- darwin/macports-setup-dev-env.sh
+++ darwin/macports-setup-dev-env.sh
@@ -8,13 +8,13 @@ getport() {
     port -v install "$@"
 }
 
-getport qt4-mac
 getport python25
 getport python_select
 
 python_select python25
 
 getport subversion
+getport py25-macholib-devel
 getport py25-py2app-devel
 getport py25-nose
 getport py25-pyqt4
