cbeae4ef7b1217004a15ccdff5ff3db9195161eb
David Aguilar <davvid@gmail.com>
2009-06-15 02:59:09 -0700

    classic: move model initialization code into the model itself
    The (currently empty) controller knew too much about the model's
    internals, so refactor that knowledge it into the model itself.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/controllers/classic.py
+++ cola/controllers/classic.py
@@ -10,49 +10,7 @@ class ClassicController(QtCore.QObject):
         QtCore.QObject.__init__(self, view)
         self._model = model
         self._view = view
-        self._dirmodel = None
-        self._dircount = {}
 
-    def initialize(self, dirmodel):
-        self._dirmodel = dirmodel
-        direntries = {'': dirmodel.root()}
-        self._dircount[''] = 0
-
-        for name in self._model.all_files():
-            dirname = os.path.dirname(name)
-            if dirname in direntries:
-                parent = direntries[dirname]
-            else:
-                parent = self._create_dir_entry(dirname, direntries)
-                direntries[dirname] = parent
-                self._dircount[dirname] = 0
-            row = self._dircount[dirname]
-            self._dircount[dirname] = row + 1
-            entry = GitDirEntry(row, [os.path.basename(name), 'file'], parent)
-            parent.add_child(entry)
-        self._view.setModel(dirmodel)
-
-    def _create_dir_entry(self, dirname, direntries):
-        # FIXME: fix row# problem for entries
-        entries = dirname.split('/')
-        curdir = []
-        parent = self._dirmodel.root()
-        for entry in entries:
-            curdir.append(entry)
-            path = '/'.join(curdir)
-            print path
-            if path in direntries:
-                parent = direntries[path]
-            else:
-                grandparent = parent
-                parent_path = '/'.join(curdir[:-1])
-                row = self._dircount[parent_path]
-                self._dircount[parent_path] = row + 1
-                parent = GitDirEntry(row, [os.path.basename(path), 'good'], parent)
-                direntries[path] = parent
-                self._dircount[path] = 0
-                grandparent.add_child(parent)
-        return parent
 
 if __name__ == '__main__':
     import sys
@@ -64,12 +22,11 @@ if __name__ == '__main__':
     dirmodel = GitDirModel(model)
 
     view = QtGui.QTreeView()
-
-    ctl = ClassicController(model, view)
-    ctl.initialize(dirmodel)
-
     view.setWindowTitle(view.tr('classic'))
+    view.setAlternatingRowColors(True)
     view.setModel(dirmodel)
+    view.resize(720, 300)
     view.show()
+    view.raise_()
 
     sys.exit(app.exec_())
--- cola/models/gitdirmodel.py
+++ cola/models/gitdirmodel.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from PyQt4 import QtCore
@@ -9,9 +10,9 @@ class GitDirModel(QtCore.QAbstractItemModel):
     """
     def __init__(self, model):
         QtCore.QAbstractItemModel.__init__(self)
-        self._headers = ('Name', 'Status')
-        self._num_columns = len(self._headers)
-        self._root = GitDirEntry(-1, self._headers)
+        self._model = model
+        self._root = GitDirEntry(name='Name', status='Status')
+        self._initialize()
 
     def parent(self, child):
         """Returns the index for the parent of a child entry"""
@@ -49,7 +50,7 @@ class GitDirModel(QtCore.QAbstractItemModel):
             return QtCore.QModelIndex()
 
         if not parent.isValid():
-            item = self._root
+            item = self.root()
         else:
             item = parent.internalPointer()
 
@@ -71,14 +72,14 @@ class GitDirModel(QtCore.QAbstractItemModel):
 
     def columnCount(self, parent=None):
         """Returns the number of columns to display"""
-        return self._num_columns
+        return self.root().num_columns()
 
     def headerData(self, section, orientation, role):
         """Returns data for the column headers"""
         if (orientation == QtCore.Qt.Horizontal and
                 role == QtCore.Qt.DisplayRole):
-            if section < len(self._headers):
-                return QtCore.QVariant(self._root.data(section))
+            if section < self.root().num_columns():
+                return QtCore.QVariant(self.root().data(section))
         return QtCore.QVariant()
 
     def rowCount(self, parent):
@@ -91,27 +92,65 @@ class GitDirModel(QtCore.QAbstractItemModel):
             item = parent.internalPointer()
         return len(item._children)
 
+    def _initialize(self):
+        direntries = {'': self.root()}
+        for name in self._model.all_files():
+            dirname = os.path.dirname(name)
+            if dirname in direntries:
+                parent = direntries[dirname]
+            else:
+                parent = self._create_dir_entry(dirname, direntries)
+                direntries[dirname] = parent
+
+            entry = GitDirEntry(os.path.basename(name), parent=parent)
+            parent.add_file(entry)
+
+    def _create_dir_entry(self, dirname, direntries):
+        entries = dirname.split('/')
+        curdir = []
+        parent = self.root()
+        for entry in entries:
+            curdir.append(entry)
+            path = '/'.join(curdir)
+            if path in direntries:
+                parent = direntries[path]
+            else:
+                grandparent = parent
+                parent_path = '/'.join(curdir[:-1])
+                parent = GitDirEntry(os.path.basename(path), parent=parent)
+                direntries[path] = parent
+                grandparent.add_directory(parent)
+        return parent
+
+
 
 class GitDirEntry(object):
     """Represents an entry in the GitDirModel
     """
-    def __init__(self, row, data, parent=None):
+    def __init__(self, name='', status='', parent=None):
         self._parent = parent
-        self._row = row # Record the item's location within its parent.
         self._children = []
-        self._data = map(QtCore.QVariant, data)
+        self._name = name
+        self._status = status
+        self._dirindex = 0
 
-    def add_child(self, child):
-        """
-        Adds a child to this entry.
-
-        Entries with children represent "tree" objects, AKA directories
+    def add_file(self, child):
+        """Add a file to this entry.
         """
         self._children.append(child)
 
+    def add_directory(self, dirent):
+        """Add a directory to this entry.
+        """
+        self._children.insert(self._dirindex, dirent)
+        self._dirindex += 1
+
+    def num_columns(self):
+        return 2
+
     def data(self, idx):
         """Returns column data for an entry"""
-        return self._data[idx]
+        return (self._name, self._status)[idx]
 
     def parent(self):
         """Returns the parent of an entry"""
@@ -129,4 +168,10 @@ class GitDirEntry(object):
 
     def row(self):
         """Returns this entry's row relative to its parent"""
-        return self._row
+        if self._parent:
+            return self._parent.child_index(self)
+        else:
+            return -1
+
+    def child_index(self, child):
+        return self._children.index(child)
