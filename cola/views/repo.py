db946108aaf8e6230373bd2273a18fabbaed7d91
David Aguilar <davvid@gmail.com>
2009-06-16 04:16:11 -0700

    views: add a RepoTreeView for use in the classic view
    Signed-off-by: David Aguilar <davvid@gmail.com>

---
+++ cola/views/repo.py
@@ -0,0 +1,49 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+from PyQt4.QtCore import Qt
+from PyQt4.QtCore import SIGNAL
+
+class RepoTreeView(QtGui.QTreeView):
+    def __init__(self, parent=None):
+        QtGui.QTreeView.__init__(self, parent)
+        self.setWindowTitle(self.tr('classic'))
+        self.setSortingEnabled(False)
+        self.setAllColumnsShowFocus(True)
+        self.setAlternatingRowColors(True)
+        self.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+        self.connect(self, SIGNAL("expanded(QModelIndex)"),
+                     lambda: self.resizeColumnToContents(0))
+
+        self.action_history = QtGui.QAction('View History...', self)
+        self.action_history.setShortcut(Qt.Key_H)
+        self.action_history.setShortcutContext(Qt.WidgetWithChildrenShortcut)
+        self.action_history.setStatusTip('Launch a history view for this path.')
+        self.addAction(self.action_history)
+        self.connect(self.action_history, SIGNAL('triggered()'),
+                     self.view_history)
+
+    def update_actions(self):
+        """Enable/disable actions."""
+        selection = self.selected_paths()
+        self.action_history.setEnabled(bool(selection))
+
+    def contextMenuEvent(self, event):
+        """Create a context menu."""
+        self.update_actions()
+        menu = QtGui.QMenu(self)
+        menu.addAction(self.action_history)
+        menu.exec_(self.mapToGlobal(event.pos()))
+
+    def setModel(self, model):
+        """Set the concrete QDirModel instance."""
+        QtGui.QTreeView.setModel(self, model)
+        self.resizeColumnToContents(0)
+
+    def selected_paths(self):
+        """Return the selected paths."""
+        return map(lambda x: x.internalPointer().path,
+                   self.selectedIndexes())
+
+    def view_history(self):
+        """Signal that we should view history for paths."""
+        self.emit(SIGNAL('history(QStringList)'), self.selected_paths())
