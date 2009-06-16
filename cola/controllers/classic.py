ccf1ca126c6d607a63e8e5d56567c3d5554c7352
David Aguilar <davvid@gmail.com>
2009-06-16 04:16:43 -0700

    classic: handle the 'history(QStringList)' signal from the RepoTreeView
    We now launch the configured history browser on the selected paths.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/controllers/classic.py
+++ cola/controllers/classic.py
@@ -1,31 +1,45 @@
-from PyQt4 import QtCore
-
 import os
 
+from PyQt4 import QtCore
+from PyQt4.QtCore import SIGNAL
+
+import cola.utils
 from cola.models.gitdirmodel import GitDirModel
 from cola.models.gitdirmodel import GitDirEntry
 
 class ClassicController(QtCore.QObject):
     def __init__(self, model, view=None):
         QtCore.QObject.__init__(self, view)
-        self._model = model
-        self._view = view
+        self.model = model
+        self.view = view
+        self.connect(view, SIGNAL('history(QStringList)'),
+                     self._view_history)
+
+
+    def _view_history(self, entries):
+        """Launch the configured history browser path-limited to entries."""
+        entries = map(unicode, entries)
+        cmd = [self.model.get_history_browser(), '--all', '--']
+        cola.utils.fork(cmd + entries)
 
 
 if __name__ == '__main__':
     import sys
     from PyQt4 import QtGui
     from cola.models import main
+    from cola.views import repo
 
     app = QtGui.QApplication(sys.argv)
     model = main.MainModel()
+    model.use_worktree(os.getcwd())
     dirmodel = GitDirModel(model)
 
-    view = QtGui.QTreeView()
-    view.setWindowTitle(view.tr('classic'))
-    view.setAlternatingRowColors(True)
+    view = repo.RepoTreeView()
     view.setModel(dirmodel)
     view.resize(720, 300)
+
+    controller = ClassicController(model, view)
+
     view.show()
     view.raise_()
 
