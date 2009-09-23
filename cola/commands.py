3002e39456dc79e5423b0d94284da6b7d234a244
David Aguilar <davvid@gmail.com>
2009-09-23 00:17:30 -0700

    commands: Add command implementations
    This libifies many of the operations cola was performing
    in the main controller.  We'll keep gutting controllers
    until we have commands for everything.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/commands.py
+++ cola/commands.py
@@ -77,122 +77,310 @@ class AddSignoff(Command):
 class AmendMode(Command):
     """Try to amend a commit."""
     def __init__(self, amend):
-        Command.__init__(self)
+        Command.__init__(self, update=True)
         self.skip = False
         self.amending = amend
-        self.old_mode = self.model.mode
-        self.old_head = self.model.head
-        self.old_msg = self.model.commitmsg
-        self.msg = ''
+        self.old_commitmsg = self.model.commitmsg
 
         if self.amending:
+            self.new_mode = self.model.mode_amend
+            self.new_head = 'HEAD^'
+            self.new_commitmsg = self.model.prev_commitmsg()
             return
+        # else, amend unchecked, regular commit
+        self.new_mode = self.model.mode_none
+        self.new_head = 'HEAD'
+        self.new_commitmsg = self.model.commitmsg
         # If we're going back into new-commit-mode then search the
         # undo stack for a previous amend-commid-mode and grab the
         # commit message at that point in time.
-        factory = cmdfactory.factory()
-        if not factory.undostack:
+        if not _factory.undostack:
             return
-        undo_count = len(factory.undostack)
+        undo_count = len(_factory.undostack)
         for i in xrange(undo_count):
+            # Find the latest AmendMode command
             idx = undo_count - i - 1
-            cmdobj = factory.undostack[idx]
+            cmdobj = _factory.undostack[idx]
             if type(cmdobj) is not AmendMode:
                 continue
             if cmdobj.amending:
-                self.msg = cmdobj.old_msg
-            break
-    
+                self.new_commitmsg = cmdobj.old_commitmsg
+                break
+
     def do(self):
         """Leave/enter amend mode."""
-        if self.amending:
-            self.enter_amend_mode()
-        else:
-            self.enter_new_commit_mode()
-
-    def enter_amend_mode(self):
         """Attempt to enter amend mode.  Do not allow this when merging."""
-        if os.path.exists(self.model.git_repo_path('MERGE_HEAD')):
-            self.skip = True
-            cola.notifier().broadcast(signals.amend, False)
-            cola.notifier().broadcast(signals.information,
-                                      'Oops! Unmerged',
-                                      'You are in the middle of a merge.\n'
-                                      'You cannot amend while merging.')
-        else:
-            self.skip = False
-            cola.notifier().broadcast(signals.amend, True)
-            self.model.set_head('HEAD^')
-            self.model.set_mode(self.model.mode_amend)
-            self.model.set_commitmsg(self.model.prev_commitmsg())
-            self.model.update_status()
-
-    def enter_new_commit_mode(self):
-        """Switch back to new-commit mode."""
-        cola.notifier().broadcast(signals.amend, False)
-        self.model.set_head('HEAD')
-        self.model.set_mode(self.model.mode_none)
-        self.model.set_commitmsg(self.msg)
-        self.model.update_status()
+        if self.amending:
+            if os.path.exists(self.model.git_repo_path('MERGE_HEAD')):
+                self.skip = True
+                _notifier.broadcast(signals.amend, False)
+                _notifier.broadcast(signals.information,
+                                    'Oops! Unmerged',
+                                    'You are in the middle of a merge.\n'
+                                    'You cannot amend while merging.')
+                return
+        self.skip = False
+        _notifier.broadcast(signals.amend, self.amending)
+        self.model.set_commitmsg(self.new_commitmsg)
+        Command.do(self)
 
     def undo(self):
         if self.skip:
             return
-        self.model.set_head(self.old_head)
-        self.model.set_mode(self.old_mode)
-        self.model.set_commitmsg(self.old_text)
-        self.model.update_status()
+        self.model.set_commitmsg(self.old_commitmsg)
+        Command.undo(self)
+
+
+class HeadChangeCommand(Command):
+    """Changes the model's current head."""
+    def __init__(self, treeish):
+        Command.__init__(self, update=True)
+        self.new_head = treeish
+        self.new_diff_text = ''
+
+
+class BranchMode(HeadChangeCommand):
+    """Enter into diff-branch mode."""
+    def __init__(self, treeish, filename):
+        HeadChangeCommand.__init__(self, treeish)
+        self.old_filename = self.model.filename
+        self.new_filename = filename
+        self.new_mode = self.model.mode_branch
+        self.new_diff_text = self.model.diff_helper(filename=filename,
+                                                    cached=False,
+                                                    reverse=True,
+                                                    branch=treeish)
+
+class Commit(Command):
+    """Attempt to create a new commit."""
+    def __init__(self, amend, msg):
+        Command.__init__(self)
+        self.amend = amend
+        self.msg = core.encode(msg)
+        self.old_commitmsg = self.model.commitmsg
+        self.new_mode = self.model.mode_none
+        self.new_commitmsg = ''
+
+    def do(self):
+        status, output = self.model.commit_with_msg(self.msg, amend=self.amend)
+        if status == 0:
+            self.model.set_mode(self.new_mode)
+            self.model.set_commitmsg(self.new_commitmsg)
+            self.model.update_status()
+            _notifier.broadcast(signals.amend, False)
+
+    def is_undoable(self):
+        return False
 
 
 class Diff(Command):
     """Perform a diff and set the model's current text."""
-    def __init__(self, filenames):
+    def __init__(self, filenames, cached=False):
         Command.__init__(self)
-        self.filenames = filenames 
-        self.old_mode = self.model.mode
-        self.old_text = self.model.current_text
+        opts = {}
+        if cached:
+            cached = not self.model.read_only()
+            opts = dict(ref=self.model.head)
+
+        self.new_filename = filenames[0]
+        self.old_filename = self.model.filename
+        if not self.model.read_only():
+            self.new_mode = self.model.mode_worktree
+        self.new_diff_text = self.model.diff_helper(filename=self.new_filename,
+                                                    cached=cached, **opts)
 
     def do(self):
-        if not self.filenames:
-            return
-        self.model.set_mode(self.model.mode_worktree)
-        filename = self.filenames[0]
-        diff = self.model.diff_helper(filename=filename,
-                                      ref=self.model.head,
-                                      cached=False)
-        self.model.set_current_text(diff)
+        self.model.set_filename(self.new_filename)
+        Command.do(self)
 
     def undo(self):
-        self.model.set_mode(self.old_mode)
-        self.model.set_current_text(self.old_text)
+        self.model.set_filename(self.old_filename)
+        Command.undo(self)
+
+
+class DiffMode(HeadChangeCommand):
+    """Enter diff mode and clear the model's diff text."""
+    def __init__(self, treeish):
+        HeadChangeCommand.__init__(self, treeish)
+        self.new_mode = self.model.mode_diff
+
+
+class DiffExprMode(HeadChangeCommand):
+    """Enter diff-expr mode and clear the model's diff text."""
+    def __init__(self, treeish):
+        HeadChangeCommand.__init__(self, treeish)
+        self.new_mode = self.model.mode_diff_expr
 
 
 class Diffstat(Command):
-    """Perform a diffstat and set the model's current text."""
+    """Perform a diffstat and set the model's diff text."""
     def __init__(self):
         Command.__init__(self)
-        self.old_text = self.model.current_text
+        diff = self.model.git.diff(self.model.head,
+                                   unified=self.model.diff_context,
+                                   no_color=True,
+                                   M=True,
+                                   stat=True)
+        self.new_diff_text = core.decode(diff)
+        self.new_mode = self.model.mode_worktree
+
+
+class DiffStaged(Diff):
+    """Perform a staged diff on a file."""
+    def __init__(self, filenames):
+        Diff.__init__(self, filenames, cached=True)
+        if not self.model.read_only():
+            if self.model.mode != self.model.mode_amend:
+                self.new_mode = self.model.mode_index
+
+
+class DiffStagedSummary(Command):
+    def __init__(self):
+        Command.__init__(self)
+        cached = not self.model.read_only()
+        diff = self.model.git.diff(self.model.head,
+                                   cached=cached,
+                                   no_color=True,
+                                   patch_with_stat=True,
+                                   M=True)
+        self.new_diff_text = core.decode(diff)
+        if not self.model.read_only():
+            if self.model.mode != self.model.mode_amend:
+                self.new_mode = self.model.mode_index
+
+class Edit(Command):
+    """Edit a file using the configured gui.editor."""
+    def __init__(self, filenames, line_number=None):
+        Command.__init__(self)
+        self.filenames = filenames
+        self.line_number = line_number
 
     def do(self):
-        self.model.set_current_text(self.model.diffstat())
+        filename = self.filenames[0]
+        if not os.path.exists(filename):
+            return
+        editor = self.model.editor()
+        if 'vi' in editor and self.line_number:
+            utils.fork([editor, filename, '+'+self.line_number])
+        else:
+            utils.fork([editor, filename])
+
+    def is_undoable(self):
+        return False
+
+
+class GrepMode(Command):
+    def __init__(self, txt):
+        """Perform a git-grep."""
+        Command.__init__(self)
+        self.new_mode = self.model.mode_grep
+        self.new_diff_text = self.model.git.grep(txt, n=True)
+
+
+class LoadCommitMessage(Command):
+    """Loads a commit message from a path."""
+    def __init__(self, path):
+        Command.__init__(self)
+        self.path = path
+        self.old_commitmsg = self.model.commitmsg
+        self.old_directory = self.model.directory
+
+    def do(self):
+        self.model.set_directory(os.path.dirname(self.path))
+        fh = open(self.path, 'r')
+        contents = core.decode(core.read_nointr(fh))
+        fh.close()
+        self.model.set_commitmsg(contents)
 
     def undo(self):
-        self.model.set_current_text(self.old_text)
+        self.model.set_commitmsg(self.old_commitmsg)
+        self.model.set_directory(self.old_directory)
+
+
+class OpenRepo(Command):
+    """Launches git-cola on a repo."""
+    def __init__(self, dirname):
+        self.dirname = utils.quote_repopath(dirname)
+
+    def do(self):
+        utils.fork(['python', sys.argv[0], '--repo', self.dirname])
+
+    def is_undoable(self):
+        return False
+
+
+class Rescan(Command):
+    """Rescans for changes."""
+    def __init__(self):
+        Command.__init__(self, update=True)
 
 
 class ResetMode(Command):
-    def __init__(self, model=None):
-        Command.__init__(self, model=model)
-        self.old_mode = self.model.mode
-        self.old_text = self.model.current_text
+    """Reset the mode and clear the model's diff text."""
+    def __init__(self):
+        Command.__init__(self, update=True)
+        self.new_mode = self.model.mode_none
+        self.new_head = 'HEAD'
+        self.new_diff_text = ''
+
+
+class ReviewBranchMode(Command):
+    """Enter into review-branch mode."""
+    def __init__(self, branch):
+        Command.__init__(self, update=True)
+        self.new_mode = self.model.mode_review
+        self.new_head = self.model.merge_base_to(branch)
+        self.new_diff_text = ''
+
+
+class ShowUntracked(Command):
+    """Show an untracked file."""
+    # We don't actually do anything other than set the mode right now.
+    # We could probably check the mimetype for the file and handle things
+    # generically.
+    def __init__(self, filenames):
+        Command.__init__(self)
+        self.new_mode = self.model.mode_worktree
+        # TODO new_diff_text = utils.file_preview(filenames[0])
+
+
+class Stage(Command):
+    def __init__(self, paths):
+        Command.__init__(self)
+        self.paths = paths
 
     def do(self):
-        self.model.set_mode(self.model.mode_none)
-        self.model.set_current_text('')
+        self.model.stage_paths(self.paths)
 
-    def undo(self):
-        self.model.set_mode(self.old_mode)
-        self.model.set_current_text(self.old_text)
+    def is_undoable(self):
+        return False
+
+
+class Unstage(Command):
+    def __init__(self, paths):
+        Command.__init__(self)
+        self.paths = paths
+
+    def do(self):
+        self.model.unstage_paths(self.paths)
+
+    def is_undoable(self):
+        return False
+
+
+class UntrackedSummary(Command):
+    """List possible .gitignore rules as the diff text."""
+    def __init__(self):
+        Command.__init__(self)
+        untracked = self.model.untracked
+        suffix = len(untracked) > 1 and 's' or ''
+        io = StringIO()
+        io.write('# %s untracked file%s\n' % (len(untracked), suffix))
+        if untracked:
+            io.write('# possible .gitignore rule%s:\n' % suffix)
+            for u in untracked:
+                io.write('/%s\n' % u)
+        self.new_diff_text = io.getvalue()
 
 
 def register():
@@ -206,12 +394,26 @@ def register():
     signal_to_command_map = {
         signals.add_signoff: AddSignoff,
         signals.amend_mode: AmendMode,
+        signals.branch_mode: BranchMode,
+        signals.commit: Commit,
         signals.diff: Diff,
+        signals.diff_mode: DiffMode,
+        signals.diff_expr_mode: DiffExprMode,
+        signals.diff_staged: DiffStaged,
         signals.diffstat: Diffstat,
+        signals.edit: Edit,
+        signals.grep: GrepMode,
         signals.modified_summary: Diffstat,
+        signals.open_repo: OpenRepo,
+        signals.rescan: Rescan,
         signals.reset_mode: ResetMode,
+        signals.review_branch_mode: ReviewBranchMode,
+        signals.show_untracked: ShowUntracked,
+        signals.stage: Stage,
+        signals.staged_summary: DiffStagedSummary,
+        signals.unstage: Unstage,
+        signals.untracked_summary: UntrackedSummary,
     }
 
-    factory = cmdfactory.factory()
     for signal, cmd in signal_to_command_map.iteritems():
-        factory.add_command(signal, cmd)
+        _factory.add_command(signal, cmd)
