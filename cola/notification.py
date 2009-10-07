36b8183822e7b01f732c96b9601a53dd1bcc7c20
David Aguilar <davvid@gmail.com>
2009-10-07 00:38:09 -0700

    notification: Replace QObject usage with a pure-Python implementation
    Qt signals+slots are limiting because they don't allow optional
    keyword arguments.  Let's write our own so we can simplify the
    Diff command later.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/notification.py
+++ cola/notification.py
@@ -8,17 +8,31 @@ def notifier():
     global _instance
     if _instance:
         return _instance
-    _instance = QNotifier()
+    _instance = Notifier()
     return _instance
 
 
-class QNotifier(QtCore.QObject):
-    def broadcast(self, signal, *args, **kwargs):
+class Notifier(object):
+    """A pure-python re-implementation of QObject."""
+    def __init__(self):
+        self.channels = {}
+
+    def broadcast(self, signal, *args, **opts):
         if debug:
             print ('broadcast: %s(%s, %s)' % (signals.name(signal),
                                               args or '<empty>',
                                               kwargs or '<empty>'))
-        self.emit(signal, *args, **kwargs)
+        self.emit(signal, *args, **opts)
+
+    def emit(self, signal, *args, **opts):
+        subscribers = self.channels.get(signal, None)
+        if subscribers:
+            for fxn in subscribers:
+                fxn(*args, **opts)
 
     def listen(self, signal, callback):
-        self.connect(self, signal, callback)
+        self.connect(signal, callback)
+
+    def connect(self, signal, callback):
+        subscribers = self.channels.setdefault(signal, set())
+        subscribers.add(callback)
