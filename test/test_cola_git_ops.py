55b453a896656dbafec2d7ee2faaeacbc563719b
David Aguilar <davvid@gmail.com>
2009-11-19 13:12:57 -0800

    test: Use assertEqual() in the git ops test
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/test_cola_git_ops.py
+++ test/test_cola_git_ops.py
@@ -17,11 +17,11 @@ class ColaBasicGitTestCase(helper.TestCase):
             git add A B
             """)
 
-        model = main.MainModel()
-        model.git.commit(m="commit test")
+        model = main.MainModel(cwd=os.getcwd())
+        model.git.commit(m='commit test')
         log = helper.pipe('git log --pretty=oneline | wc -l')
 
-        self.failUnless('1' == log)
+        self.assertEqual(log.strip(), '1')
 
     def test_git_config(self):
         """Test cola.git.config()"""
@@ -29,28 +29,28 @@ class ColaBasicGitTestCase(helper.TestCase):
             git init 2>&1 >/dev/null
             git config section.key value
         """)
-        model = main.MainModel()
+        model = main.MainModel(cwd=os.getcwd())
         value = model.git.config('section.key', get=True)
 
-        self.failUnless(value == 'value')
+        self.assertEqual(value, 'value')
 
         #  Test config_set
         model.config_set('section.bool', True)
         value = model.git.config('section.bool', get=True)
 
-        self.failUnless(value == 'true')
+        self.assertEqual(value, 'true')
         model.config_set('section.bool', False)
 
         # Test config_dict
         config_dict = model.config_dict(local=True)
 
-        self.failUnless(config_dict['section_key'] == 'value')
-        self.failUnless(config_dict['section_bool'] == False)
+        self.assertEqual(config_dict['section_key'], 'value')
+        self.assertEqual(config_dict['section_bool'], False)
 
         # Test config_dict --global
         global_dict = model.config_dict(local=False)
 
-        self.failUnless(type(global_dict) is dict)
+        self.assertEqual(type(global_dict), dict)
 
 if __name__ == '__main__':
     unittest.main()
