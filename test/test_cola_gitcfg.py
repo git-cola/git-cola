8485311a4f0675e36db5f94a686e8bd99d700d6e
David Aguilar <davvid@gmail.com>
2009-11-20 16:05:57 -0800

    test: Add a testcase for int values from GitConfig.get()
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/test_cola_gitcfg.py
+++ test/test_cola_gitcfg.py
@@ -15,6 +15,11 @@ class GitConfigTestCase(helper.GitRepositoryTestCase):
         self.shell('git config test.value test')
         self.assertEqual(self.config.get('test.value'), 'test')
 
+    def test_int(self):
+        """Test int values in get()."""
+        self.shell('git config test.int 42')
+        self.assertEqual(self.config.get('test.int'), 42)
+
 
 if __name__ == '__main__':
     unittest.main()
