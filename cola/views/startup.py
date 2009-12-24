7cdcc37bd42ade79d70974d2b5e8a684a9dc0513
Daniel King <cnkknd@gmail.com>
2009-12-24 15:37:12 +0800

    Add bookmark list to startup window.

--- cola/views/startup.py
+++ cola/views/startup.py
@@ -12,7 +12,7 @@ from PyQt4.QtCore import SIGNAL
 
 from cola import qtutils
 from cola import guicmds
-
+from cola import settings
 
 class StartupDialog(QtGui.QDialog):
     """Provides a GUI to Open or Clone a git repository."""
@@ -30,7 +30,29 @@ class StartupDialog(QtGui.QDialog):
         self._layt.addWidget(self._open_btn)
         self._layt.addWidget(self._clone_btn)
         self._layt.addWidget(self._close_btn)
-        self.setLayout(self._layt)
+
+	self.model = settings.SettingsManager.settings()
+
+	if(len(self.model.bookmarks) > 0):
+	    self._vlayt = QtGui.QVBoxLayout()
+            self._bookmark_model = QtGui.QStandardItemModel()
+            item = QtGui.QStandardItem('Select manually...')
+            item.setEditable(False)
+            self._bookmark_model.appendRow(item)
+            for bookmark in self.model.bookmarks:
+                item = QtGui.QStandardItem(bookmark)
+                item.setEditable(False)
+                self._bookmark_model.appendRow(item)
+	    self._bookmark_list = QtGui.QListView()
+            self._bookmark_list.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+            self._bookmark_list.setModel(self._bookmark_model)
+            self._vlayt.addWidget(self._bookmark_list)
+	    self._vlayt.addLayout(self._layt)
+            self.resize(400, 150)
+            self.setLayout(self._vlayt)
+            self._bookmark_list.activated.connect(self._open_bookmark)
+        else:
+            self.setLayout(self._layt)
 
         self.connect(self._open_btn, SIGNAL('clicked()'), self._open)
         self.connect(self._clone_btn, SIGNAL('clicked()'), self._clone)
@@ -53,7 +75,9 @@ class StartupDialog(QtGui.QDialog):
         return None
 
     def _open(self):
-        self._gitdir = qtutils.opendir_dialog(self,
+        self._gitdir = self._get_selected_bookmark()
+        if not self._gitdir:
+            self._gitdir = qtutils.opendir_dialog(self,
                                               'Open Git Repository...',
                                               os.getcwd())
         if self._gitdir:
@@ -64,3 +88,17 @@ class StartupDialog(QtGui.QDialog):
         if gitdir:
             self._gitdir = gitdir
             self.accept()
+
+    def _open_bookmark(self, index):
+        if(index.row() == 0):
+            self._open()
+        else:
+            self._gitdir = unicode(self._bookmark_model.data(index).toString())
+            if self._gitdir:
+                self.accept()
+
+    def _get_selected_bookmark(self):
+        selected = self._bookmark_list.selectedIndexes()
+        if(len(selected) > 0 and selected[0].row() != 0):
+            return unicode(self._bookmark_model.data(selected[0]).toString())
+        return None
