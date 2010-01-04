7d3d60322bd8d1ec7383a6b55e82c871b35c6ebe
David Aguilar <davvid@gmail.com>
2010-01-04 10:55:12 -0800

    cola: Add cola.serializer for handling object serialization
    This will contain the serialization functionality currently
    coupled to the Model class.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

---
+++ cola/serializer.py
@@ -0,0 +1,52 @@
+"""Provides a serializer for arbitrary Python objects"""
+import jsonpickle
+jsonpickle.set_encoder_options('simplejson', indent=4)
+
+from cola import utils
+
+handlers = {}
+
+
+def save(obj, path):
+    utils.write(path, encode(obj))
+
+
+def load(path):
+    import jsonpickle
+    return decode(utils.slurp(path))
+
+
+def clone(obj):
+    # Go in and out of encode/decode to return a clone
+    return decode(encode(obj))
+
+
+def encode(obj):
+    handler = _gethandler(obj)
+    if handler:
+        handler.pre_encode_hook()
+    jsonstr = jsonpickle.encode(obj)
+    if handler:
+        handler.post_encode_hook()
+    return jsonstr
+
+
+def decode(jsonstr):
+    obj = jsonpickle.decode(jsonstr)
+    handler = _gethandler(obj)
+    if handler:
+        handler.post_decode_hook()
+    return obj
+
+
+def _gethandler(obj):
+    cls = type(obj)
+    # Allow base classes to provide a serialization handlers
+    # for their subclasses
+    if hasattr(cls, 'mro'):
+        for supercls in cls.mro():
+            if supercls in handlers:
+                return handlers[supercls](obj)
+    if cls in handlers:
+        return handlers[cls](obj)
+    return None
