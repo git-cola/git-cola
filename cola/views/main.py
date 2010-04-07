25044f6138b8345695e861e25e03ac1fb686488f
David Aguilar <davvid@gmail.com>
2010-04-06 20:52:03 -0700

    views.main: Unparent the DAG and classic apps
    This lets us put these in the background, etc.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/views/main.py
+++ cola/views/main.py
@@ -155,8 +155,8 @@ class MainView(MainWindow):
             (self.menu_select_all, self.commitmsg.selectAll),
             (self.menu_undo, self.commitmsg.undo),
             (self.menu_redo, self.commitmsg.redo),
-            (self.menu_classic, lambda: classic.cola_classic(self)),
-            (self.menu_dag, lambda: dag.git_dag(parent=self)),
+            (self.menu_classic, classic.cola_classic),
+            (self.menu_dag, dag.git_dag),
         )
         for menu, callback in actions:
             self.connect(menu, SIGNAL('triggered()'), callback)
