d684ed1e57c8af56060e606eda497446d01b3920
David Aguilar <davvid@gmail.com>
2010-04-21 01:59:08 -0700

    Allow configuration via config.sh
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- common.sh
+++ common.sh
@@ -14,6 +14,10 @@ RELEASE_TREE_RELEASES="$RELEASE_TREE"/releases
 RELEASE_TREE_WIN32="$RELEASE_TREE_RELEASES"/win32
 RELEASE_TREE_DARWIN="$RELEASE_TREE_RELEASES"/darwin
 
+if test -e "$META"/config.sh
+then
+	. "$META"/config.sh
+fi
 
 do_or_die() {
 	if ! "$@"; then
