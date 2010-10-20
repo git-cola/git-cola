dd6bb1acb20d3ff4b049d351d27a24a74bd05967
David Aguilar <davvid@gmail.com>
2010-10-20 00:25:11 -0700

    qt: Check layouts against None instead of assuming bool(layout) is True
    PyQt 4.7.3 no longer evaluates bool(layout) to True.
    Thanks to the patient Ubuntu and Arch users who reported this problem.
    
    Closes #62
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/qt.py
+++ cola/qt.py
@@ -8,7 +8,7 @@ def create_button(text, layout=None):
     """Create a button, set its title, and add it to the parent."""
     button = QtGui.QPushButton()
     button.setText(i18n.gettext(text))
-    if layout:
+    if layout is not None:
         layout.addWidget(button)
     return button
 
