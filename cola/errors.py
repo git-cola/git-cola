814227f984213a0b083f7ee17a9b4d3236256b41
David Aguilar <davvid@gmail.com>
2011-02-26 23:27:17 -0800

    errors: Add 'UsageError' for use by command objects
    UsageError represents usage errors.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/errors.py
+++ cola/errors.py
@@ -3,11 +3,20 @@ class ColaError(StandardError):
     """The base class of all cola exceptions"""
     pass
 
+
 class GitCommandError(ColaError):
     """Exception class for failed commands."""
     pass
+
 
 class GitInitError(ColaError):
     """Exception class for errors related to just-initialized repositories."""
     pass
 
+
+class UsageError(ColaError):
+    """Exception class for usage errors."""
+    def __init__(self, title, message):
+        ColaError.__init__(self, message)
+        self.title = title
+        self.message = message
