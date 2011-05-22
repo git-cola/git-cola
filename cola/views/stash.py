b08e125c871193c58a62eeb597f0a316cf4c7f37
David Aguilar <davvid@gmail.com>
2011-05-22 08:44:45 -0700

    stash: Use a nicer layout in the stash GUI
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/views/stash.py
+++ cola/views/stash.py
@@ -19,21 +19,27 @@ class StashView(standard.StandardDialog):
         self._label.setText('<center>Stash List</center>')
 
         self._main_layt = QtGui.QVBoxLayout(self)
-        self._main_layt.setMargin(5)
-        self._main_layt.setSpacing(5)
-        self._main_layt.addWidget(self._label)
+        self._main_layt.setMargin(4)
+        self._main_layt.setSpacing(2)
 
         # Exposed
         self.stash_list = QtGui.QListWidget(self)
-        self._main_layt.addWidget(self.stash_list)
+
+        self._btn_layt = QtGui.QVBoxLayout()
+        self._btn_layt.setMargin(0)
+        self._btn_layt.setSpacing(2)
+
+        self._btn_layt0 = QtGui.QHBoxLayout()
+        self._btn_layt0.setMargin(0)
+        self._btn_layt0.setSpacing(4)
 
         self._btn_layt1 = QtGui.QHBoxLayout()
         self._btn_layt1.setMargin(0)
-        self._btn_layt1.setSpacing(1)
+        self._btn_layt1.setSpacing(4)
 
         self._btn_layt2 = QtGui.QHBoxLayout()
         self._btn_layt2.setMargin(0)
-        self._btn_layt2.setSpacing(1)
+        self._btn_layt2.setSpacing(4)
 
         # Exposed
         self.button_stash_show = QtGui.QPushButton(self)
@@ -41,35 +47,29 @@ class StashView(standard.StandardDialog):
                 self.tr('Show the changes recorded in the '
                         'selected stash stash as a diff'))
         self.button_stash_show.setText(self.tr('Show'))
-        self._btn_layt1.addWidget(self.button_stash_show)
 
         # Exposed
         self.button_stash_apply = QtGui.QPushButton(self)
         self.button_stash_apply.setToolTip(self.tr('Apply the selected stash'))
         self.button_stash_apply.setText(self.tr('Apply'))
-        self._btn_layt1.addWidget(self.button_stash_apply)
 
         # Exposed
         self.button_stash_save = QtGui.QPushButton(self)
         self.button_stash_save.setText(self.tr('Save'))
-        self._btn_layt1.addWidget(self.button_stash_save)
 
         # Exposed
         self.button_stash_drop = QtGui.QPushButton(self)
         self.button_stash_drop.setToolTip(self.tr('Remove the selected stash'))
         self.button_stash_drop.setText(self.tr('Remove'))
-        self._btn_layt2.addWidget(self.button_stash_drop)
 
         # Exposed
         self.button_stash_clear = QtGui.QPushButton(self)
         self.button_stash_clear.setToolTip(self.tr('Remove all stashed states'))
         self.button_stash_clear.setText(self.tr('Remove All'))
-        self._btn_layt2.addWidget(self.button_stash_clear)
 
         # Exposed
         self.button_stash_done = QtGui.QPushButton(self)
         self.button_stash_done.setText(self.tr('Done'))
-        self._btn_layt2.addWidget(self.button_stash_done)
 
         # Exposed
         self.keep_index = QtGui.QCheckBox(self)
@@ -81,9 +81,25 @@ class StashView(standard.StandardDialog):
         self.setTabOrder(self.button_stash_apply, self.button_stash_drop)
         self.setTabOrder(self.button_stash_drop, self.button_stash_clear)
 
-        self._main_layt.addWidget(self.keep_index)
-        self._main_layt.addItem(self._btn_layt1)
-        self._main_layt.addItem(self._btn_layt2)
+        # Arrange layouts
+        self._btn_layt0.addStretch()
+        self._btn_layt0.addWidget(self.keep_index)
+
+        self._btn_layt1.addWidget(self.button_stash_show)
+        self._btn_layt1.addWidget(self.button_stash_apply)
+        self._btn_layt1.addWidget(self.button_stash_save)
+
+        self._btn_layt2.addWidget(self.button_stash_drop)
+        self._btn_layt2.addWidget(self.button_stash_clear)
+        self._btn_layt2.addWidget(self.button_stash_done)
+
+        self._btn_layt.addItem(self._btn_layt0)
+        self._btn_layt.addItem(self._btn_layt1)
+        self._btn_layt.addItem(self._btn_layt2)
+
+        self._main_layt.addWidget(self._label)
+        self._main_layt.addWidget(self.stash_list)
+        self._main_layt.addItem(self._btn_layt)
         self._main_layt.addStretch()
 
 
