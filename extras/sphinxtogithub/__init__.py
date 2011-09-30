40cb20bef075fbb6624f948902bb903caf4ee91c
David Aguilar <davvid@gmail.com>
2011-09-30 00:03:00 -0700

    doc: Add sphinxtogithub extension
    Signed-off-by: David Aguilar <davvid@gmail.com>

---
+++ extras/sphinxtogithub/__init__.py
@@ -0,0 +1,29 @@
+"""Script for preparing the html output of the Sphinx documentation system for
+github pages. """
+
+VERSION = (1, 0, 0, 'dev')
+
+__version__ = ".".join(map(str, VERSION[:-1]))
+__release__ = ".".join(map(str, VERSION))
+__author__ = "Michael Jones"
+__contact__ = "http://github.com/michaeljones"
+__homepage__ = "http://github.com/michaeljones/sphinx-to-github"
+__docformat__ = "restructuredtext"
+
+from sphinxtogithub import (
+    setup,
+    sphinx_extension,
+    LayoutFactory,
+    Layout,
+    DirectoryHandler,
+    VerboseRename,
+    ForceRename,
+    Remover,
+    FileHandler,
+    Replacer,
+    DirHelper,
+    FileSystemHelper,
+    OperationsFactory,
+    HandlerFactory
+)
+
