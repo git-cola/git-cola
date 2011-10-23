9518515adbc3083c8d979bef573b593a77d3aa3e
David Aguilar <davvid@gmail.com>
2011-10-22 20:08:24 -0700

    dag: Register command wrappers when standalone
    git-dag will use this soon.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/dag/__init__.py
+++ cola/dag/__init__.py
@@ -9,16 +9,19 @@ if __name__ == '__main__':
     sys.path.insert(1, os.path.join(os.path.abspath(src), 'thirdparty'))
     sys.path.insert(1, os.path.abspath(src))
 
+from cola import guicmds
 from cola.dag.view import GitDAGWidget
 from cola.dag.model import DAG
 from cola.dag.controller import GitDAGController
 
 
-def git_dag(model, parent):
+def git_dag(model, parent, standalone=False):
     """Return a pre-populated git DAG widget."""
     model = DAG(model.currentbranch, 1000)
     view = GitDAGWidget(model, parent=parent)
     ctl = GitDAGController(model, view)
+    if standalone:
+        guicmds.install_command_wrapper(view)
     view.resize_to_desktop()
     view.show()
     view.raise_()
@@ -35,5 +38,5 @@ if __name__ == "__main__":
     model.update_status()
 
     app = app.ColaApplication(sys.argv)
-    ctl = git_dag(model, app.activeWindow())
+    ctl = git_dag(model, app.activeWindow(), standalone=True)
     sys.exit(app.exec_())
