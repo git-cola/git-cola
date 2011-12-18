1dfea63dbce99da990e8be5ca9ebf1393551580b
David Aguilar <davvid@gmail.com>
2011-12-17 21:22:12 -0800

    widgets.search: Improve design and usability
    Get rid of all the radio buttons and make it as simple as possible.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/search.py
+++ cola/widgets/search.py
@@ -1,6 +1,5 @@
 """Provides the SearchView class."""
 import os
-import re
 import time
 import subprocess
 
@@ -12,20 +11,12 @@ from cola import gitcmds
 from cola import utils
 from cola import qtutils
 from cola.git import git
-from cola.prefs import diff_font
-from cola.qt import DiffSyntaxHighlighter
+from cola.qt import create_toolbutton
 from cola.qtutils import connect_button
+from cola.qtutils import dir_icon
 from cola.views import standard
 from cola.widgets import defs
-
-REVISION_ID    = 'revision'
-REVISION_RANGE = 'range'
-PATH           = 'path'
-MESSAGE        = 'message'
-DIFF           = 'diff'
-AUTHOR         = 'author'
-COMMITTER      = 'committer'
-DATE_RANGE     = 'daterange'
+from cola.widgets.diff import DiffTextEdit
 
 
 def mkdate(timespec):
@@ -35,7 +26,7 @@ def mkdate(timespec):
 class SearchOptions(object):
     def __init__(self):
         self.query = ''
-        self.max_results = 500
+        self.max_count = 500
         self.start_date = ''
         self.end_date = ''
 
@@ -44,116 +35,87 @@ class SearchWidget(standard.Dialog):
     def __init__(self, parent):
         super(SearchWidget, self).__init__(parent)
         self.setWindowTitle(self.tr('Search'))
-        self.main_layout = QtGui.QVBoxLayout()
-        self.setLayout(self.main_layout)
-        self.resize(600, self.parent().height())
 
-        self.top_grid_layout = QtGui.QGridLayout()
-
-        self.radio_range = QtGui.QRadioButton()
-        self.radio_range.setText(self.tr('Revision Range Expression'))
-        self.top_grid_layout.addWidget(self.radio_range, 0, 0, 1, 2)
-
-        self.radio_path = QtGui.QRadioButton()
-        self.radio_path.setText(self.tr('Commits Touching Paths'))
-        self.top_grid_layout.addWidget(self.radio_path, 0, 2, 1, 1)
-
-        self.radio_author = QtGui.QRadioButton()
-        self.radio_author.setText(self.tr('Author'))
-        self.top_grid_layout.addWidget(self.radio_author, 0, 3, 1, 1)
-
-        self.radio_committer = QtGui.QRadioButton()
-        self.radio_committer.setText(self.tr('Committer'))
-        self.top_grid_layout.addWidget(self.radio_committer, 0, 4, 1, 1)
-
-        self.radio_revision = QtGui.QRadioButton()
-        self.radio_revision.setText(self.tr('Revision ID'))
-        self.top_grid_layout.addWidget(self.radio_revision, 1, 0, 1, 2)
-
-        self.radio_message = QtGui.QRadioButton()
-        self.radio_message.setText(self.tr('Commit Messages'))
-        self.top_grid_layout.addWidget(self.radio_message, 1, 2, 1, 1)
-
-        self.radio_daterange = QtGui.QRadioButton()
-        self.radio_daterange.setText(self.tr('Date Range (Start / End)'))
-        self.top_grid_layout.addWidget(self.radio_daterange, 1, 3, 1, 2)
-
-        self.maxresults_label = QtGui.QLabel()
-        self.maxresults_label.setText(self.tr('Max Results'))
-        self.top_grid_layout.addWidget(self.maxresults_label, 2, 0, 1, 1)
-
-        self.max_results = QtGui.QSpinBox()
-        self.max_results.setMinimum(5)
-        self.max_results.setMaximum(9995)
-        self.max_results.setSingleStep(5)
-        self.max_results.setProperty('value', QtCore.QVariant(500))
-        self.top_grid_layout.addWidget(self.max_results, 2, 1, 1, 1)
-
-        self.radio_diff = QtGui.QRadioButton()
-        self.radio_diff.setText(self.tr('Diff Content'))
-        self.top_grid_layout.addWidget(self.radio_diff, 2, 2, 1, 1)
+        self.mode_combo = QtGui.QComboBox()
+        self.browse_button = create_toolbutton(icon=dir_icon(),
+                                               tooltip='Browse...')
+        self.query = QtGui.QLineEdit()
 
         self.start_date = QtGui.QDateEdit()
         self.start_date.setCurrentSection(QtGui.QDateTimeEdit.YearSection)
         self.start_date.setCalendarPopup(True)
         self.start_date.setDisplayFormat(self.tr('yyyy-MM-dd'))
-        self.top_grid_layout.addWidget(self.start_date, 2, 3, 1, 1)
 
         self.end_date = QtGui.QDateEdit()
         self.end_date.setCurrentSection(QtGui.QDateTimeEdit.YearSection)
         self.end_date.setCalendarPopup(True)
         self.end_date.setDisplayFormat(self.tr('yyyy-MM-dd'))
-        self.top_grid_layout.addWidget(self.end_date, 2, 4, 1, 1)
 
-        self.button_search = QtGui.QPushButton()
-        self.button_search.setText(self.tr('Search'))
-        self.button_search.setShortcut(self.tr('Return'))
-        self.top_grid_layout.addWidget(self.button_search, 3, 0, 1, 1)
+        self.search_button = QtGui.QPushButton()
+        self.search_button.setText(self.tr('Search'))
+        self.search_button.setDefault(True)
 
-        self.query = QtGui.QLineEdit()
-        self.top_grid_layout.addWidget(self.query, 3, 1, 1, 3)
+        self.max_count = QtGui.QSpinBox()
+        self.max_count.setMinimum(5)
+        self.max_count.setMaximum(9995)
+        self.max_count.setSingleStep(5)
+        self.max_count.setValue(500)
 
-        self.button_browse = QtGui.QPushButton()
-        self.button_browse.setText(self.tr('Browse'))
-        self.top_grid_layout.addWidget(self.button_browse, 3, 4, 1, 1)
-        self.main_layout.addLayout(self.top_grid_layout)
-
-        self.splitter = QtGui.QSplitter()
-        self.splitter.setHandleWidth(defs.handle_width)
-        self.splitter.setOrientation(QtCore.Qt.Vertical)
-
-        self.commit_list = QtGui.QListWidget(self.splitter)
+        self.commit_list = QtGui.QListWidget()
         self.commit_list.setMinimumSize(QtCore.QSize(1, 1))
         self.commit_list.setAlternatingRowColors(True)
         self.commit_list.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
 
-        self.commit_text = QtGui.QTextEdit(self.splitter)
-        self.commit_text.setMinimumSize(QtCore.QSize(1, 1))
+        self.commit_text = DiffTextEdit(self, whitespace=False)
         self.commit_text.setTabChangesFocus(True)
         self.commit_text.setReadOnly(True)
-        self.commit_text.setAcceptRichText(False)
-        self.commit_text.setFont(diff_font())
-        self._syntax = DiffSyntaxHighlighter(self.commit_text.document(),
-                                             whitespace=False)
-        self.main_layout.addWidget(self.splitter)
-
-        self._button_grid_layt = QtGui.QGridLayout()
 
         self.button_export = QtGui.QPushButton()
         self.button_export.setText(self.tr('Export Patches'))
-        self._button_grid_layt.addWidget(self.button_export, 0, 0, 1, 1)
 
         self.button_cherrypick = QtGui.QPushButton()
         self.button_cherrypick.setText(self.tr('Cherry Pick'))
-        self._button_grid_layt.addWidget(self.button_cherrypick, 0, 1, 1, 1)
-
-        self._button_spacer = QtGui.QSpacerItem(111, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self._button_grid_layt.addItem(self._button_spacer, 0, 2, 1, 1)
 
         self.button_close = QtGui.QPushButton()
         self.button_close.setText(self.tr('Close'))
-        self._button_grid_layt.addWidget(self.button_close, 0, 3, 1, 1)
-        self.main_layout.addLayout(self._button_grid_layt)
+
+        self.top_layout = QtGui.QHBoxLayout()
+        self.top_layout.setMargin(0)
+        self.top_layout.setSpacing(defs.button_spacing)
+
+        self.top_layout.addWidget(self.browse_button)
+        self.top_layout.addWidget(self.query)
+        self.top_layout.addWidget(self.start_date)
+        self.top_layout.addWidget(self.end_date)
+        self.top_layout.addWidget(self.search_button)
+        self.top_layout.addStretch()
+        self.top_layout.addWidget(self.mode_combo)
+        self.top_layout.addWidget(self.max_count)
+
+        self.splitter = QtGui.QSplitter()
+        self.splitter.setHandleWidth(defs.handle_width)
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.splitter.setChildrenCollapsible(True)
+        self.splitter.addWidget(self.commit_list)
+        self.splitter.addWidget(self.commit_text)
+
+        self.bottom_layout = QtGui.QHBoxLayout()
+        self.bottom_layout.setMargin(0)
+        self.bottom_layout.setSpacing(defs.spacing)
+        self.bottom_layout.addWidget(self.button_export)
+        self.bottom_layout.addWidget(self.button_cherrypick)
+        self.bottom_layout.addStretch()
+        self.bottom_layout.addWidget(self.button_close)
+
+        self.main_layout = QtGui.QVBoxLayout()
+        self.main_layout.setMargin(defs.margin)
+        self.main_layout.setSpacing(defs.spacing)
+        self.main_layout.addLayout(self.top_layout)
+        self.main_layout.addWidget(self.splitter)
+        self.main_layout.addLayout(self.bottom_layout)
+        self.setLayout(self.main_layout)
+
+        self.resize(self.parent().width(), self.parent().height())
 
 
 def search():
@@ -166,7 +128,7 @@ class SearchEngine(object):
         self.model = model
 
     def rev_args(self):
-        max_count = self.model.max_results
+        max_count = self.model.max_count
         return {
             'no_color': True,
             'max-count': max_count,
@@ -193,22 +155,9 @@ class SearchEngine(object):
 
 class RevisionSearch(SearchEngine):
     def results(self):
-        query, args = self.common_args()
-        expr = re.compile(query)
-        revs = self.revisions(all=True, **args)
-        return [r for r in revs if expr.match(r[0])]
-
-class RevisionRangeSearch(SearchEngine):
-    def __init__(self, model):
-        SearchEngine.__init__(self, model)
-        self.RE = re.compile(r'[^.]*\.\..*')
-
-    def validate(self):
-        return bool(self.RE.match(self.model.query))
-
-    def results(self):
-        query, kwargs = self.common_args()
-        return self.revisions(query, **kwargs)
+        query, opts = self.common_args()
+        args = utils.shell_split(query)
+        return self.revisions(all=True, *args, **opts)
 
 
 class PathSearch(SearchEngine):
@@ -258,42 +207,42 @@ class DateRangeSearch(SearchEngine):
                               **kwargs)
 
 
-# Each search type is handled by a distinct SearchEngine subclass
-SEARCH_ENGINES = {
-    REVISION_ID:    RevisionSearch,
-    REVISION_RANGE: RevisionRangeSearch,
-    PATH:           PathSearch,
-    MESSAGE:        MessageSearch,
-    DIFF:           DiffSearch,
-    AUTHOR:         AuthorSearch,
-    COMMITTER:      CommitterSearch,
-    DATE_RANGE:     DateRangeSearch,
-}
-
 class Search(SearchWidget):
+    EXPR            = 'Search by Expression'
+    PATH            = 'Search by Path'
+    MESSAGE         = 'Search Commit Messages'
+    DIFF            = 'Search Diffs'
+    AUTHOR          = 'Search Authors'
+    COMMITTER       = 'Search Committers'
+    DATE_RANGE      = 'Search Date Range'
+
     def __init__(self, model, parent):
         super(Search, self).__init__(parent)
         self.model = model
-        self.radiobuttons = {
-                REVISION_ID: self.radio_revision,
-                REVISION_RANGE: self.radio_range,
-                PATH: self.radio_path,
-                MESSAGE: self.radio_message,
-                DIFF: self.radio_diff,
-                AUTHOR: self.radio_author,
-                COMMITTER: self.radio_committer,
-                DATE_RANGE: self.radio_daterange,
+
+        # Each search type is handled by a distinct SearchEngine subclass
+        self.engines = {
+            self.EXPR:           RevisionSearch,
+            self.PATH:           PathSearch,
+            self.MESSAGE:        MessageSearch,
+            self.DIFF:           DiffSearch,
+            self.AUTHOR:         AuthorSearch,
+            self.COMMITTER:      CommitterSearch,
+            self.DATE_RANGE:     DateRangeSearch,
         }
 
-        self.radiobuttons_to_mode = {}
-        for k, v in self.radiobuttons.items():
-            self.radiobuttons_to_mode[v] = k
+        self.modes = (self.EXPR, self.PATH, self.DATE_RANGE,
+                      self.DIFF, self.MESSAGE, self.AUTHOR, self.COMMITTER)
+        self.mode_combo.addItems(self.modes)
 
-        connect_button(self.button_search, self.search_callback)
-        connect_button(self.button_browse, self.browse_callback)
+        connect_button(self.search_button, self.search_callback)
+        connect_button(self.browse_button, self.browse_callback)
         connect_button(self.button_export, self.export_patch)
         connect_button(self.button_cherrypick, self.cherry_pick)
         connect_button(self.button_close, self.accept)
+
+        self.connect(self.mode_combo, SIGNAL('currentIndexChanged(int)'),
+                     self.mode_index_changed)
 
         self.connect(self.commit_list,
                      SIGNAL('itemSelectionChanged()'),
@@ -301,9 +250,15 @@ class Search(SearchWidget):
 
         self.set_start_date(mkdate(time.time()-(87640*31)))
         self.set_end_date(mkdate(time.time()+87640))
-        self.set_mode(REVISION_ID)
+        self.set_mode(self.EXPR)
 
         self.query.setFocus()
+
+    def mode_index_changed(self, idx):
+        mode = self.mode()
+        self.update_shown_widgets(mode)
+        if mode == self.PATH:
+            self.browse_callback()
 
     def set_commit_list(self, commits):
         widget = self.commit_list
@@ -323,19 +278,25 @@ class Search(SearchWidget):
             widget.setDate(date)
 
     def set_mode(self, mode):
-        radio = self.radiobuttons[mode]
-        radio.setChecked(True)
+        idx = self.modes.index(mode)
+        self.mode_combo.setCurrentIndex(idx)
+        self.update_shown_widgets(mode)
+
+    def update_shown_widgets(self, mode):
+        date_shown = mode == self.DATE_RANGE
+        browse_shown = mode == self.PATH
+        self.query.setVisible(not date_shown)
+        self.browse_button.setVisible(browse_shown)
+        self.start_date.setVisible(date_shown)
+        self.end_date.setVisible(date_shown)
 
     def mode(self):
-        for radio, mode in self.radiobuttons_to_mode.items():
-            if radio.isChecked():
-                return mode
-        return REVISION_ID
+        return str(self.mode_combo.currentText())
 
     def search_callback(self, *args):
-        engineclass = SEARCH_ENGINES[self.mode()]
+        engineclass = self.engines[self.mode()]
         self.model.query = unicode(self.query.text())
-        self.model.max_results = self.max_results.value()
+        self.model.max_count = self.max_count.value()
 
         fmt = QtCore.Qt.ISODate
         self.model.start_date = str(self.start_date.date().toString(fmt))
@@ -360,8 +321,9 @@ class Search(SearchWidget):
                 continue
             filepaths.append(path[lenprefix:])
         query = subprocess.list2cmdline(filepaths)
-        self.set_mode(PATH)
         self.query.setText(query)
+        if query:
+            self.search_callback()
 
     def display_results(self):
         commit_list = map(lambda x: x[1], self.results)
