331619b1281d7b3178c11ccb5f49e797f6f92907
David Aguilar <davvid@gmail.com>
2011-12-28 00:14:12 -0800

    dag: Draw slightly thicker edges
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/dag/view.py
+++ cola/dag/view.py
@@ -515,7 +515,7 @@ class Edge(QtGui.QGraphicsItem):
     arrow_size = 2.0
     arrow_extra = (arrow_size+1.0)/2.0
 
-    pen = QtGui.QPen(QtCore.Qt.gray, 1.0,
+    pen = QtGui.QPen(QtCore.Qt.gray, 1.3,
                      QtCore.Qt.DotLine,
                      QtCore.Qt.SquareCap,
                      QtCore.Qt.BevelJoin)
