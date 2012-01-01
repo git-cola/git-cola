b90d34fcf0da37b083da95e20ffc5fa8e9dcd3b2
David Aguilar <davvid@gmail.com>
2011-12-31 20:27:24 -0800

    widgets.commitmsg: Factor out the 'hint' text behavior
    Create standalone widgets to display the 'Commit summary' and
    'Extended description...' text hints.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/commitmsg.py
+++ cola/widgets/commitmsg.py
@@ -23,6 +23,8 @@ from cola.prefs import diff_font
 from cola.dag.model import DAG
 from cola.dag.model import RepoReader
 from cola.widgets.selectcommits import select_commits
+from cola.widgets.text import HintedLineEdit
+from cola.widgets.text import HintedTextEdit
 
 
 class CommitMessageEditor(QtGui.QWidget):
@@ -31,18 +33,6 @@ class CommitMessageEditor(QtGui.QWidget):
 
         self.model = model
         self.notifying = False
-        self.summary_placeholder = u'Commit summary'
-        self.description_placeholder = u'Extended description...'
-
-        # Palette for normal text
-        self.default_palette = QtGui.QPalette(self.palette())
-
-        # Palette used for the placeholder text
-        self.placeholder_palette = pal = QtGui.QPalette(self.palette())
-        color = pal.text().color()
-        color.setAlpha(128)
-        pal.setColor(QtGui.QPalette.Active, QtGui.QPalette.Text, color)
-        pal.setColor(QtGui.QPalette.Inactive, QtGui.QPalette.Text, color)
 
         self.summary = CommitSummaryLineEdit()
         self.description = CommitMessageTextEdit()
@@ -107,8 +97,12 @@ class CommitMessageEditor(QtGui.QWidget):
         self.connect(self.summary, SIGNAL('returnPressed()'),
                      self.focus_description)
 
-        self.connect(self.summary, SIGNAL('cursorPositionChanged(int,int)'),
-                     lambda x, y: self.emit_summary_position())
+        self.connect(self.summary, SIGNAL('cursorPosition(int,int)'),
+                     self.emit_position)
+
+        self.connect(self.description, SIGNAL('cursorPosition(int,int)'),
+                     # description starts at line 2
+                     lambda row, col: self.emit_position(row + 2, col))
 
         # Keep model informed of changes
         self.connect(self.summary, SIGNAL('textChanged(QString)'),
@@ -117,19 +111,13 @@ class CommitMessageEditor(QtGui.QWidget):
         self.connect(self.description, SIGNAL('textChanged()'),
                      self.commit_message_changed)
 
-        self.connect(self.description, SIGNAL('cursorPositionChanged()'),
-                     self.emit_cursor_position)
-
         self.connect(self.description, SIGNAL('shiftTab()'),
                      self.focus_summary)
 
         self.setFont(diff_font())
 
-        self.summary.installEventFilter(self)
-        self.description.installEventFilter(self)
-
-        self.enable_placeholder_summary(True)
-        self.enable_placeholder_description(True)
+        self.summary.enable_hint(True)
+        self.description.enable_hint(True)
 
         self.commit_button.setEnabled(False)
         self.commit_action.setEnabled(False)
@@ -139,85 +127,13 @@ class CommitMessageEditor(QtGui.QWidget):
     def focus_summary(self):
         self.summary.setFocus(True)
 
-    def is_summary_placeholder(self):
-        summary = unicode(self.summary.text()).strip()
-        return summary == self.summary_placeholder
-
-    def is_description_placeholder(self):
-        description = unicode(self.description.toPlainText()).strip()
-        return description == self.description_placeholder
-
-    def eventFilter(self, obj, event):
-        if obj == self.summary:
-            if event.type() == QtCore.QEvent.FocusIn:
-                self.emit_summary_position()
-                if self.is_summary_placeholder():
-                    self.enable_placeholder_summary(False)
-
-            elif event.type() == QtCore.QEvent.FocusOut:
-                if not bool(self.commit_summary()):
-                    self.enable_placeholder_summary(True)
-
-        elif obj == self.description:
-            if event.type() == QtCore.QEvent.FocusIn:
-                self.emit_cursor_position()
-                if self.is_description_placeholder():
-                    self.enable_placeholder_description(False)
-
-            elif event.type() == QtCore.QEvent.FocusOut:
-                if not bool(self.commit_description()):
-                    self.enable_placeholder_description(True)
-
-        return False
-
-    def enable_placeholder_summary(self, placeholder):
-        blocksignals = self.summary.blockSignals(True)
-        if placeholder:
-            self.summary.setText(self.summary_placeholder)
-        else:
-            self.summary.clear()
-        self.summary.setCursorPosition(0)
-        self.summary.blockSignals(blocksignals)
-        self.set_placeholder_palette(self.summary, placeholder)
-
-    def enable_placeholder_description(self, placeholder):
-        blocksignals = self.description.blockSignals(True)
-        if placeholder:
-            self.description.setPlainText(self.description_placeholder)
-        else:
-            self.description.clear()
-        self.description.blockSignals(blocksignals)
-        self.set_placeholder_palette(self.description, placeholder)
-
-    def set_placeholder_palette(self, widget, placeholder):
-        if placeholder:
-            widget.setPalette(self.placeholder_palette)
-        else:
-            widget.setPalette(self.default_palette)
-
     def focus_description(self):
         self.description.setFocus(True)
 
-    def commit_summary(self):
-        """Return the commit summary as unicode"""
-        summary = unicode(self.summary.text()).strip()
-        if summary != self.summary_placeholder:
-            return summary
-        else:
-            return u''
-
-    def commit_description(self):
-        """Return the commit description as unicode"""
-        description = unicode(self.description.toPlainText()).strip()
-        if description != self.description_placeholder:
-            return description
-        else:
-            return u''
-
     def commit_message(self):
         """Return the commit message as a unicode string"""
-        summary = self.commit_summary()
-        description = self.commit_description()
+        summary = self.summary.value()
+        description = self.description.value()
         if summary and description:
             return summary + u'\n\n' + description
         elif summary:
@@ -232,21 +148,19 @@ class CommitMessageEditor(QtGui.QWidget):
         self.notifying = True
         message = self.commit_message()
         self.model.set_commitmsg(message)
-        self.update_placeholder_state()
+        self.refresh_palettes()
         self.notifying = False
         self.update_actions()
 
     def update_actions(self):
-        commit_enabled = bool(self.commit_summary())
+        commit_enabled = bool(self.summary.value())
         self.commit_button.setEnabled(commit_enabled)
         self.commit_action.setEnabled(commit_enabled)
 
-    def update_placeholder_state(self):
-        """Update the color palette for the placeholder text"""
-        self.set_placeholder_palette(self.summary,
-                                     self.is_summary_placeholder())
-        self.set_placeholder_palette(self.description,
-                                     self.is_description_placeholder())
+    def refresh_palettes(self):
+        """Update the color palette for the hint text"""
+        self.summary.refresh_palette()
+        self.description.refresh_palette()
 
     def set_commit_message(self, message):
         """Set the commit message to match the observed model"""
@@ -291,7 +205,7 @@ class CommitMessageEditor(QtGui.QWidget):
 
         # Update summary
         if not summary and not self.summary.hasFocus():
-            summary = self.summary_placeholder
+            summary = self.summary.hint()
 
         blocksignals = self.summary.blockSignals(True)
         self.summary.setText(summary)
@@ -300,24 +214,24 @@ class CommitMessageEditor(QtGui.QWidget):
 
         # Update description
         if not description and not self.description.hasFocus():
-            description = self.description_placeholder
+            description = self.description.hint()
 
         blocksignals = self.description.blockSignals(True)
         self.description.setPlainText(description)
         self.description.blockSignals(blocksignals)
 
         # Update text color
-        self.update_placeholder_state()
+        self.refresh_palettes()
 
         # Focus the empty summary or description
         if focus_summary:
             self.summary.setFocus(True)
-            self.emit_summary_position()
+            self.summary.emit_position()
         elif focus_description:
             self.description.setFocus(True)
-            self.emit_cursor_position()
+            self.description.emit_position()
         else:
-            self.emit_summary_position()
+            self.summary.emit_position()
 
         self.update_actions()
 
@@ -332,22 +246,12 @@ class CommitMessageEditor(QtGui.QWidget):
         self.amend_action.setChecked(checked)
         self.amend_action.blockSignals(blocksignals)
 
-    def emit_summary_position(self):
-        cols = self.summary.cursorPosition()
-        self.emit(SIGNAL('cursorPosition(int,int)'), 1, cols)
-
-    def emit_cursor_position(self):
-        """Update the UI with the current row and column."""
-        cursor = self.description.textCursor()
-        position = cursor.position()
-        txt = unicode(self.description.toPlainText())
-        rows = txt[:position].count('\n') + 1 + 2 # description starts at 2
-        cols = cursor.columnNumber()
-        self.emit(SIGNAL('cursorPosition(int,int)'), rows, cols)
+    def emit_position(self, row, col):
+        self.emit(SIGNAL('cursorPosition(int,int)'), row, col)
 
     def commit(self):
         """Attempt to create a commit from the index and commit message."""
-        if not bool(self.commit_summary()):
+        if not bool(self.summary.value()):
             # Describe a good commit message
             error_msg = tr(''
                 'Please supply a commit message.\n\n'
@@ -431,9 +335,10 @@ class CommitMessageEditor(QtGui.QWidget):
         self.emit(SIGNAL(signals.load_previous_message), sha1)
 
 
-class CommitSummaryLineEdit(QtGui.QLineEdit):
+class CommitSummaryLineEdit(HintedLineEdit):
     def __init__(self, parent=None):
-        super(CommitSummaryLineEdit, self).__init__(parent)
+        hint = u'Commit summary'
+        super(CommitSummaryLineEdit, self).__init__(hint, parent)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Down:
@@ -446,11 +351,10 @@ class CommitSummaryLineEdit(QtGui.QLineEdit):
         super(CommitSummaryLineEdit, self).keyPressEvent(event)
 
 
-class CommitMessageTextEdit(QtGui.QTextEdit):
+class CommitMessageTextEdit(HintedTextEdit):
     def __init__(self, parent=None):
-        QtGui.QTextEdit.__init__(self, parent)
-        self.setLineWrapMode(QtGui.QTextEdit.NoWrap)
-        self.setAcceptRichText(False)
+        hint = u'Extended description...'
+        super(CommitMessageTextEdit, self).__init__(hint, parent)
         self.setMinimumSize(QtCore.QSize(1, 1))
 
         self.action_emit_shift_tab = add_action(self,
---
+++ cola/widgets/text.py
@@ -0,0 +1,128 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+from PyQt4.QtCore import SIGNAL
+
+
+class HintedTextWidgetEventFilter(QtCore.QObject):
+    def __init__(self, parent):
+        super(HintedTextWidgetEventFilter, self).__init__(parent)
+        self.widget = parent
+
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.FocusIn:
+            self.widget.emit_position()
+            if self.widget.is_hint():
+                self.widget.enable_hint(False)
+
+        elif event.type() == QtCore.QEvent.FocusOut:
+            if not bool(self.widget.value()):
+                self.widget.enable_hint(True)
+
+        return False
+
+
+class HintedTextWidgetMixin(object):
+    def __init__(self, hint):
+        self._hint = hint
+        self._event_filter = HintedTextWidgetEventFilter(self)
+        self.installEventFilter(self._event_filter)
+
+        # Palette for normal text
+        self.default_palette = QtGui.QPalette(self.palette())
+
+        # Palette used for the placeholder text
+        self.hint_palette = pal = QtGui.QPalette(self.palette())
+        color = self.hint_palette.text().color()
+        color.setAlpha(128)
+        pal.setColor(QtGui.QPalette.Active, QtGui.QPalette.Text, color)
+        pal.setColor(QtGui.QPalette.Inactive, QtGui.QPalette.Text, color)
+
+    def emit_position(self):
+        pass
+
+    def reset_cursor(self):
+        pass
+
+    def hint(self):
+        return self._hint
+
+    def is_hint(self):
+        return self.strip() == self._hint
+
+    def value(self):
+        text = self.strip()
+        if text == self._hint:
+            return u''
+        else:
+            return text
+
+    def strip(self):
+        return self.as_unicode().strip()
+
+    def enable_hint(self, hint):
+        blocksignals = self.blockSignals(True)
+        if hint:
+            self.set_value(self.hint())
+        else:
+            self.clear()
+        self.reset_cursor()
+        self.blockSignals(blocksignals)
+        self.enable_hint_palette(hint)
+
+    def enable_hint_palette(self, hint):
+        if hint:
+            self.setPalette(self.hint_palette)
+        else:
+            self.setPalette(self.default_palette)
+
+    def refresh_palette(self):
+        self.enable_hint_palette(self.is_hint())
+
+
+class HintedTextEdit(QtGui.QTextEdit, HintedTextWidgetMixin):
+    def __init__(self, hint, parent=None):
+        QtGui.QTextEdit.__init__(self, parent)
+        HintedTextWidgetMixin.__init__(self, hint)
+
+        self.setLineWrapMode(QtGui.QTextEdit.NoWrap)
+        self.setAcceptRichText(False)
+
+        self.connect(self, SIGNAL('cursorPositionChanged()'),
+                     self.emit_position)
+
+    def as_unicode(self):
+        return unicode(self.toPlainText())
+
+    def set_value(self, value):
+        self.setPlainText(value)
+
+    def emit_position(self):
+        cursor = self.textCursor()
+        position = cursor.position()
+        txt = self.as_unicode()
+        rows = txt[:position].count('\n') + 1
+        cols = cursor.columnNumber()
+        self.emit(SIGNAL('cursorPosition(int,int)'), rows, cols)
+
+
+class HintedLineEdit(QtGui.QLineEdit, HintedTextWidgetMixin):
+    def __init__(self, hint, parent=None):
+        QtGui.QLineEdit.__init__(self, parent)
+        HintedTextWidgetMixin.__init__(self, hint)
+
+        self.connect(self,
+                     SIGNAL('cursorPositionChanged(int,int)'),
+                     lambda x, y: self.emit_position())
+
+    def emit_position(self):
+        cols = self.cursorPosition()
+        self.emit(SIGNAL('cursorPosition(int,int)'), 1, cols)
+
+    def set_value(self, value):
+        self.setText(value)
+
+    def as_unicode(self):
+        return unicode(self.text())
+
+    def reset_cursor(self):
+        self.setCursorPosition(0)
