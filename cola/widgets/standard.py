f0cc19d9410f97d013e6c00cb8b6b70f00abe081
David Aguilar <davvid@gmail.com>
2013-04-27 23:54:35 -0700

    widgets.standard: simplify how we bind mixins
    We had a huge class inside a function, which is unsightly.
    
    Make it simpler by pulling the functionality out into a mixin and use a
    tiny closure to bind the mixin with each Qt-specific widget class.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/standard.py
+++ cola/widgets/standard.py
@@ -4,155 +4,159 @@ from PyQt4.QtCore import Qt
 from PyQt4.QtCore import SIGNAL
 
 
-def create_widget_class(Base):
+class WidgetMixin(object):
 
-    class Widget(Base):
+    # not exported
+    def __init__(self, QtClass):
+        self.QtClass = QtClass
 
-        # Mix-in for standard view operations
-        def __init__(self, parent=None):
-            Base.__init__(self, parent)
+    # Mix-in for standard view operations
+    def show(self):
+        """Automatically centers dialogs"""
+        if self.parent():
+            left = self.parent().x()
+            width = self.parent().width()
+            center_x = left + width/2
 
-        def show(self):
-            """Automatically centers dialogs"""
-            if self.parent():
-                left = self.parent().x()
-                width = self.parent().width()
-                center_x = left + width/2
+            x = center_x - self.width()/2
+            y = self.parent().y()
 
-                x = center_x - self.width()/2
-                y = self.parent().y()
+            self.move(x, y)
+        # Call the base Qt show()
+        return self.QtClass.show(self)
 
-                self.move(x, y)
-            # Call the base Qt show()
-            Base.show(self)
+    def name(self):
+        """Returns the name of the view class"""
+        return self.__class__.__name__.lower()
 
-        def name(self):
-            """Returns the name of the view class"""
-            return self.__class__.__name__.lower()
-
-        def apply_state(self, state):
-            """Imports data for view save/restore"""
-            try:
-                self.resize(state['width'], state['height'])
-            except:
-                pass
-            try:
-                self.move(state['x'], state['y'])
-            except:
-                pass
-            if state.get('maximized', False):
-                self.showMaximized()
-
-        def export_state(self):
-            """Exports data for view save/restore"""
-            state = self.windowState()
-            maximized = bool(state & Qt.WindowMaximized)
-            return {
-                'x': self.x(),
-                'y': self.y(),
-                'width': self.width(),
-                'height': self.height(),
-                'maximized': maximized,
-            }
-
-    return Widget
-
-
-def create_tree_class(Base):
-
-    class Tree(Base):
-        def __init__(self, parent=None):
-            Base.__init__(self, parent)
-            self.setAlternatingRowColors(True)
-            self.setUniformRowHeights(True)
-            self.setAllColumnsShowFocus(True)
-            self.setAnimated(True)
-
-        def key_pressed(self):
+    def apply_state(self, state):
+        """Imports data for view save/restore"""
+        try:
+            self.resize(state['width'], state['height'])
+        except:
             pass
+        try:
+            self.move(state['x'], state['y'])
+        except:
+            pass
+        if state.get('maximized', False):
+            self.showMaximized()
 
-        def keyPressEvent(self, event):
-            """
-            Make LeftArrow to work on non-directories.
+    def export_state(self):
+        """Exports data for view save/restore"""
+        state = self.windowState()
+        maximized = bool(state & Qt.WindowMaximized)
+        return {
+            'x': self.x(),
+            'y': self.y(),
+            'width': self.width(),
+            'height': self.height(),
+            'maximized': maximized,
+        }
 
-            When LeftArrow is pressed on a file entry or an unexpanded
-            directory, then move the current index to the parent directory.
 
-            This simplifies navigation using the keyboard.
-            For power-users, we support Vim keybindings ;-P
+class TreeMixin(object):
 
-            """
-            # Check whether the item is expanded before calling the base class
-            # keyPressEvent otherwise we end up collapsing and changing the
-            # current index in one shot, which we don't want to do.
-            index = self.currentIndex()
-            was_expanded = self.isExpanded(index)
-            was_collapsed = not was_expanded
+    def __init__(self, QtClass):
+        self.QtClass = QtClass
+        self.setAlternatingRowColors(True)
+        self.setUniformRowHeights(True)
+        self.setAllColumnsShowFocus(True)
+        self.setAnimated(True)
 
-            # Vim keybindings...
-            # Rewrite the event before marshalling to QTreeView.event()
-            key = event.key()
+    def keyPressEvent(self, event):
+        """
+        Make LeftArrow to work on non-directories.
 
-            # Remap 'H' to 'Left'
-            if key == Qt.Key_H:
-                event = QtGui.QKeyEvent(event.type(),
-                                        Qt.Key_Left,
-                                        event.modifiers())
-            # Remap 'J' to 'Down'
-            elif key == Qt.Key_J:
-                event = QtGui.QKeyEvent(event.type(),
-                                        Qt.Key_Down,
-                                        event.modifiers())
-            # Remap 'K' to 'Up'
-            elif key == Qt.Key_K:
-                event = QtGui.QKeyEvent(event.type(),
-                                        Qt.Key_Up,
-                                        event.modifiers())
-            # Remap 'L' to 'Right'
-            elif key == Qt.Key_L:
-                event = QtGui.QKeyEvent(event.type(),
-                                        Qt.Key_Right,
-                                        event.modifiers())
+        When LeftArrow is pressed on a file entry or an unexpanded
+        directory, then move the current index to the parent directory.
 
-            # Re-read the event key to take the remappings into account
-            key = event.key()
+        This simplifies navigation using the keyboard.
+        For power-users, we support Vim keybindings ;-P
 
-            result = Base.keyPressEvent(self, event)
+        """
+        # Check whether the item is expanded before calling the base class
+        # keyPressEvent otherwise we end up collapsing and changing the
+        # current index in one shot, which we don't want to do.
+        index = self.currentIndex()
+        was_expanded = self.isExpanded(index)
+        was_collapsed = not was_expanded
 
-            # Let others hook in here before we change the indexes
-            self.emit(SIGNAL('indexAboutToChange()'))
+        # Vim keybindings...
+        # Rewrite the event before marshalling to QTreeView.event()
+        key = event.key()
 
-            # Try to select the first item if the model index is invalid
-            if not index.isValid():
-                index = self.model().index(0, 0, QtCore.QModelIndex())
-                if index.isValid():
-                    self.setCurrentIndex(index)
+        # Remap 'H' to 'Left'
+        if key == Qt.Key_H:
+            event = QtGui.QKeyEvent(event.type(),
+                                    Qt.Key_Left,
+                                    event.modifiers())
+        # Remap 'J' to 'Down'
+        elif key == Qt.Key_J:
+            event = QtGui.QKeyEvent(event.type(),
+                                    Qt.Key_Down,
+                                    event.modifiers())
+        # Remap 'K' to 'Up'
+        elif key == Qt.Key_K:
+            event = QtGui.QKeyEvent(event.type(),
+                                    Qt.Key_Up,
+                                    event.modifiers())
+        # Remap 'L' to 'Right'
+        elif key == Qt.Key_L:
+            event = QtGui.QKeyEvent(event.type(),
+                                    Qt.Key_Right,
+                                    event.modifiers())
 
-            # Automatically select the first entry when expanding a directory
-            elif (key == Qt.Key_Right and was_collapsed and
-                    self.isExpanded(index)):
-                index = self.moveCursor(self.MoveDown, event.modifiers())
+        # Re-read the event key to take the remappings into account
+        key = event.key()
+
+        result = self.QtClass.keyPressEvent(self, event)
+
+        # Let others hook in here before we change the indexes
+        self.emit(SIGNAL('indexAboutToChange()'))
+
+        # Try to select the first item if the model index is invalid
+        if not index.isValid():
+            index = self.model().index(0, 0, QtCore.QModelIndex())
+            if index.isValid():
                 self.setCurrentIndex(index)
 
-            # Process non-root entries with valid parents only.
-            elif key == Qt.Key_Left and index.parent().isValid():
+        # Automatically select the first entry when expanding a directory
+        elif (key == Qt.Key_Right and was_collapsed and
+                self.isExpanded(index)):
+            index = self.moveCursor(self.MoveDown, event.modifiers())
+            self.setCurrentIndex(index)
 
-                # File entries have rowCount() == 0
-                if self.model().itemFromIndex(index).rowCount() == 0:
-                    self.setCurrentIndex(index.parent())
+        # Process non-root entries with valid parents only.
+        elif key == Qt.Key_Left and index.parent().isValid():
 
-                # Otherwise, do this for collapsed directories only
-                elif was_collapsed:
-                    self.setCurrentIndex(index.parent())
+            # File entries have rowCount() == 0
+            if self.model().itemFromIndex(index).rowCount() == 0:
+                self.setCurrentIndex(index.parent())
 
-            return result
-    # Tree is a closure over "Base"
-    return Tree
+            # Otherwise, do this for collapsed directories only
+            elif was_collapsed:
+                self.setCurrentIndex(index.parent())
+
+        return result
 
 
-Widget = create_widget_class(QtGui.QWidget)
-Dialog = create_widget_class(QtGui.QDialog)
-MainWindow = create_widget_class(QtGui.QMainWindow)
+def bind_mixin(Mixin, QtClass):
+    """Construct a class which composes the Mixin over the Qt class"""
 
-TreeView = create_tree_class(QtGui.QTreeView)
-TreeWidget = create_tree_class(QtGui.QTreeWidget)
+    class BoundMixin(Mixin, QtClass):
+        """A concrete class tied to a specific Qt class"""
+
+        def __init__(self, parent=None):
+            QtClass.__init__(self, parent)
+            Mixin.__init__(self, QtClass)
+
+    return BoundMixin
+
+
+Widget = bind_mixin(WidgetMixin, QtGui.QWidget)
+Dialog = bind_mixin(WidgetMixin, QtGui.QDialog)
+MainWindow = bind_mixin(WidgetMixin, QtGui.QMainWindow)
+
+TreeView = bind_mixin(TreeMixin, QtGui.QTreeView)
+TreeWidget = bind_mixin(TreeMixin, QtGui.QTreeWidget)
