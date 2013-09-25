d925e943547af9b871640c7b20b2fe58e325c4ec
David Aguilar <davvid@gmail.com>
2013-09-24 22:26:30 -0700

    release: simplify workflow
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- common.sh
+++ common.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 unset CDPATH
-
-curdir="$(pwd)"
+top=$(git rev-parse --show-toplevel)
+curdir=$(pwd)
 cd "$(dirname "$0")"
-META="$(pwd)"
+META=$(pwd)
 cd "$curdir"
 unset curdir
 
+WEB_SOURCE="$top/../git-cola.github.com"
 # _the_ cola version
 if test -e bin/git-cola
 then
 	VERSION=$(bin/git-cola version | awk '{print $3}')
 fi
 
-WEB_SOURCE="$HOME"/src/cola-web
-
-if test -e "$META"/config.sh
+if test -e "$META/config.sh"
 then
-	. "$META"/config.sh
+	. "$META/config.sh"
 fi
 
 do_or_die() {
@@ -30,11 +29,9 @@ do_or_die() {
 }
 
 title() {
-	echo
 	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 	echo "    $@"
 	echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-	echo
 }
 
 ensure_dir_exists() {
