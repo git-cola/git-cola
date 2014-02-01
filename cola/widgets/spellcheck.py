3923f61c6b5d1f4d8ca0191ea80b8f4682c3377d
Alex Chernetz <andy80586@gmail.com>
2014-01-31 22:47:14 -0500

    widgets/spellcheck: Fix indentation missing a space in edits1()

--- cola/widgets/spellcheck.py
+++ cola/widgets/spellcheck.py
@@ -36,12 +36,12 @@ def train(features, model):
 
 
 def edits1(word):
-   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-   deletes    = [a + b[1:] for a, b in splits if b]
-   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-   inserts    = [a + c + b     for a, b in splits for c in alphabet]
-   return set(deletes + transposes + replaces + inserts)
+    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    deletes    = [a + b[1:] for a, b in splits if b]
+    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+    replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+    inserts    = [a + c + b     for a, b in splits for c in alphabet]
+    return set(deletes + transposes + replaces + inserts)
 
 
 def known_edits2(word, words):
