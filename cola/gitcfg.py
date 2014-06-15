d611ba03dfb1c8a31147ca3724a64bf291075408
David Aguilar <davvid@gmail.com>
2014-06-14 22:53:21 -0700

    gitcfg: add a built-in config reader
    We may be able to use this to avoid calling "git config".  This code is
    not enabled by default, but it was useful while debugging win32 unicode
    issues.
    
    It might be worth using it if we find that it improves startup time.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/gitcfg.py
+++ cola/gitcfg.py
@@ -2,6 +2,8 @@ from __future__ import division, absolute_import, unicode_literals
 
 import copy
 import fnmatch
+import os
+import re
 from os.path import join
 
 from cola import core
@@ -10,6 +12,8 @@ from cola import observable
 from cola.decorators import memoize
 from cola.git import STDOUT
 from cola.compat import ustr
+
+BUILTIN_READER = os.environ.get('GIT_COLA_BUILTIN_CONFIG_READER', False)
 
 @memoize
 def instance():
@@ -49,6 +53,34 @@ def _cache_key():
         except OSError:
             continue
     return mtimes
+
+
+def _config_to_python(v):
+    """Convert a Git config string into a Python value"""
+
+    if v in ('true', 'yes'):
+        v = True
+    elif v in ('false', 'no'):
+        v = False
+    else:
+        try:
+            v = int(v)
+        except ValueError:
+            pass
+    return v
+
+
+def _config_key_value(line, splitchar):
+    """Split a config line into a (key, value) pair"""
+
+    try:
+        k, v = line.split(splitchar, 1)
+    except ValueError:
+        # the user has a emptyentry in their git config,
+        # which Git interprets as meaning "true"
+        k = line
+        v = 'true'
+    return k, _config_to_python(v)
 
 
 class GitConfig(observable.Observable):
@@ -160,31 +192,52 @@ class GitConfig(observable.Observable):
 
     def read_config(self, path):
         """Return git config data from a path as a dictionary."""
+
+        if BUILTIN_READER:
+            return self._read_config_file(path)
+
         dest = {}
         args = ('--null', '--file', path, '--list')
         config_lines = self.git.config(*args)[STDOUT].split('\0')
         for line in config_lines:
-            try:
-                k, v = line.split('\n', 1)
-            except ValueError:
+            if not line:
                 # the user has an invalid entry in their git config
-                if not line:
-                    continue
-                k = line
-                v = 'true'
-
-            if v in ('true', 'yes'):
-                v = True
-            elif v in ('false', 'no'):
-                v = False
-            else:
-                try:
-                    v = int(v)
-                except ValueError:
-                    pass
+                continue
+            k, v = _config_key_value(line, '\n')
             self._map[k.lower()] = k
             dest[k] = v
         return dest
+
+    def _read_config_file(self, path):
+        """Read a .gitconfig file into a dict"""
+
+        config = {}
+        header_simple = re.compile(r'^\[(\s+)]$')
+        header_subkey = re.compile(r'^\[(\s+) "(\s+)"\]$')
+
+        with core.xopen(path, 'rt') as f:
+            lines = filter(bool, [line.strip() for line in f.readlines()])
+
+        prefix = ''
+        for line in lines:
+            if line.startswith('#'):
+                continue
+
+            match = header_simple.match(line)
+            if match:
+                prefix = match.group(1) + '.'
+                continue
+            match = header_subkey.match(line)
+            if match:
+                prefix = match.group(1) + '.' + match.group(2) + '.'
+                continue
+
+            k, v = _config_key_value(line, '=')
+            k = prefix + k
+            self._map[k.lower()] = k
+            config[k] = v
+
+        return config
 
     def _get(self, src, key, default):
         self.update()
