b200ba508249cd41176daddfd9a339a50947e299
mmargoliono <lie.r.min.g@gmail.com>
2014-11-08 18:16:55 +1100

    gui: update branch name in title after renaming a branch
    After renaming current branch, the UI is never notified of the model
    change.
    
    Update the current branch reference after renaming a branch. This is
    done even when the renamed branch is not currently active one. The
    operation should not be expensive, and doing so will simplify the code
    logic.
    
    Closes #369
    Reported-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
    Signed-off-by: Minarto Margoliono <lie.r.min.g@gmail.com>

--- cola/models/main.py
+++ cola/models/main.py
@@ -237,7 +237,10 @@ class MainModel(Observable):
 
     def rename_branch(self, branch, new_branch):
         status, out, err = self.git.branch(branch, new_branch, M=True)
+        self.notify_observers(self.message_about_to_update)
         self._update_branches_and_tags()
+        self._update_branch_heads()
+        self.notify_observers(self.message_updated)
         return status, out, err
 
     def _sliced_op(self, input_items, map_fn):
