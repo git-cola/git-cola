git-cola/git-cola

commit 2cbd38e2edca6ae6aa732b7afb809fb4b2a7dbf0

models/main: track deleted files
git-cola was using the core.exists() function in several places to
determine whether or not a file had been deleted.  However, in certain
corner cases, this could result in incorrect behavior.  First, the
gitcmds.diff_helper() function only checked for the existence of a file
if the cached flag was True.  So, if a user ran rm <file> instead of
git rm <file>, the diff view would not have the "deleted file" header at
the top of the diff for the unstaged file.  Also, if the user ran
git rm <file> and then recreated the file (now untracked), the icon in
the Staged list would incorrectly change from the removed icon to the
staged icon.  Finally, if git-cola was used to amend a commit with
deleted files, they would be incorrectly displayed with the staged icon
instead of the removed icon.  Fix these problems by allowing the main
model to keep sets of staged and unstaged deleted files, and adapt
several places that used core.exists() function to consult the
appropriate set instead.

Signed-off-by: Daniel Harding <dharding@living180.net>

cola/widgets/status.py
@@ -401,11 +401,12 @@ class StatusTreeWidget(QtGui.QTreeWidget):
     def set_staged(self, items):
         """Adds items to the 'Staged' subtree."""
         self._set_subtree(items, self.idx_staged, staged=True,
-                          check=not self.m.amending())
+                          deleted_set=self.m.staged_deleted)
 
     def set_modified(self, items):
         """Adds items to the 'Modified' subtree."""
-        self._set_subtree(items, self.idx_modified)
+        self._set_subtree(items, self.idx_modified,
+                          deleted_set=self.m.unstaged_deleted)
 
     def set_unmerged(self, items):
         """Adds items to the 'Unmerged' subtree."""
@@ -418,7 +419,7 @@ class StatusTreeWidget(QtGui.QTreeWidget):
     def _set_subtree(self, items, idx,
                      staged=False,
                      untracked=False,
-                     check=True):
+                     deleted_set=None):
         """Add a list of items to a treewidget item."""
         self.blockSignals(True)
         parent = self.topLevelItem(idx)
@@ -433,9 +434,10 @@ class StatusTreeWidget(QtGui.QTreeWidget):
             pass
 
         for item in items:
+            deleted = (deleted_set is not None and item in deleted_set)
             treeitem = qtutils.create_treeitem(item,
                                                staged=staged,
-                                               check=check,
+                                               deleted=deleted,
                                                untracked=untracked)
             parent.addChild(treeitem)
         self.expand_items(idx, items)
@@ -522,8 +524,8 @@ class StatusTreeWidget(QtGui.QTreeWidget):
             action.setShortcut(cmds.Unstage.SHORTCUT)
 
         # Do all of the selected items exist?
-        staged_items = self.staged_items()
-        all_exist = all([i.exists for i in staged_items])
+        all_exist = all(not i in self.m.staged_deleted and core.exists(i)
+                        for i in self.staged())
 
         if all_exist:
             menu.addAction(self.launch_editor_action)
@@ -609,8 +611,8 @@ class StatusTreeWidget(QtGui.QTreeWidget):
             action.setShortcut(cmds.Stage.SHORTCUT)
 
         # Do all of the selected items exist?
-        unstaged_items = self.unstaged_items()
-        all_exist = all([i.exists for i in unstaged_items])
+        all_exist = all(not i in self.m.unstaged_deleted and core.exists(i)
+                        for i in self.staged())
 
         if all_exist and self.unstaged():
             menu.addAction(self.launch_editor_action)
@@ -837,17 +839,21 @@ class StatusTreeWidget(QtGui.QTreeWidget):
             cmds.do(cls)
         # A staged file
         elif category == self.idx_staged:
-            cmds.do(cmds.DiffStaged, self.staged()[0])
+            item = self.staged_items()[0]
+            cmds.do(cmds.DiffStaged, item.path, item.deleted)
 
         # A modified file
         elif category == self.idx_modified:
-            cmds.do(cmds.Diff, self.modified()[0])
+            item = self.modified_items()[0]
+            cmds.do(cmds.Diff, item.path, item.deleted)
 
         elif category == self.idx_unmerged:
-            cmds.do(cmds.Diff, self.unmerged()[0])
+            item = self.unmerged_items()[0]
+            cmds.do(cmds.Diff, item.path)
 
         elif category == self.idx_untracked:
-            cmds.do(cmds.ShowUntracked, self.unstaged()[0])
+            item = self.unstaged_items()[0]
+            cmds.do(cmds.ShowUntracked, item.path)
 
     def move_up(self):
         idx = self.selected_idx()
@@ -900,7 +906,9 @@ class StatusTreeWidget(QtGui.QTreeWidget):
 
     def mimeData(self, items):
         """Return a list of absolute-path URLs"""
-        paths = qtutils.paths_from_items(items, item_filter=lambda x: x.exists)
+        paths = qtutils.paths_from_items(items, item_filter=lambda item:
+                                                    not item.deleted
+                                                    and core.exists(item.path))
         return qtutils.mimedata_from_paths(paths)
 
     def mimeTypes(self):

cola/cmds.py
@@ -646,7 +646,7 @@ class DeleteRemoteBranch(Command):
 class Diff(Command):
     """Perform a diff and set the model's current text."""
 
-    def __init__(self, filename, cached=False):
+    def __init__(self, filename, cached=False, deleted=False):
         Command.__init__(self)
         opts = {}
         if cached:
@@ -654,7 +654,9 @@ class Diff(Command):
         self.new_filename = filename
         self.new_mode = self.model.mode_worktree
         self.new_diff_text = gitcmds.diff_helper(filename=filename,
-                                                 cached=cached, **opts)
+                                                 cached=cached,
+                                                 deleted=deleted,
+                                                 **opts)
 
 
 class Diffstat(Command):
@@ -677,8 +679,8 @@ class Diffstat(Command):
 class DiffStaged(Diff):
     """Perform a staged diff on a file."""
 
-    def __init__(self, filename):
-        Diff.__init__(self, filename, cached=True)
+    def __init__(self, filename, deleted=None):
+        Diff.__init__(self, filename, cached=True, deleted=deleted)
         self.new_mode = self.model.mode_index
 
 

cola/gitcmds.py
@@ -279,6 +279,7 @@ def diff_helper(commit=None,
                 endref=None,
                 filename=None,
                 cached=True,
+                deleted=False,
                 head=None,
                 amending=False,
                 with_diff_header=False,
@@ -306,11 +307,6 @@ def diff_helper(commit=None,
             argv.append(filename)
             cfg = gitcfg.current()
             encoding = cfg.file_encoding(filename)
-
-    if filename is not None:
-        deleted = cached and not core.exists(filename)
-    else:
-        deleted = False
 
     status, out, err = git.diff(R=reverse, M=True, cached=cached,
                                 _encoding=encoding,
@@ -445,8 +441,9 @@ def worktree_state(head='HEAD',
     if update_index:
         git.update_index(refresh=True)
 
-    staged, unmerged, staged_submods = diff_index(head, paths=paths)
-    modified, modified_submods = diff_worktree(paths)
+    staged, unmerged, staged_deleted, staged_submods = diff_index(head,
+                                                                  paths=paths)
+    modified, unstaged_deleted, modified_submods = diff_worktree(paths)
     untracked = display_untracked and untracked_files(paths=paths) or []
 
     # Remove unmerged paths from the modified list
@@ -469,6 +466,8 @@ def worktree_state(head='HEAD',
             'unmerged': unmerged,
             'untracked': untracked,
             'upstream_changed': upstream_changed,
+            'staged_deleted': staged_deleted,
+            'unstaged_deleted': unstaged_deleted,
             'submodules': staged_submods | modified_submods}
 
 
@@ -481,9 +480,10 @@ def _parse_raw_diff(out):
 
 
 def diff_index(head, cached=True, paths=None):
-    submodules = set()
     staged = []
     unmerged = []
+    deleted = set()
+    submodules = set()
 
     if paths is None:
         paths = []
@@ -499,14 +499,17 @@ def diff_index(head, cached=True, paths=None):
             submodules.add(path)
         if status in 'DAMT':
             staged.append(path)
+            if status == 'D':
+                deleted.add(path)
         elif status == 'U':
             unmerged.append(path)
 
-    return staged, unmerged, submodules
+    return staged, unmerged, deleted, submodules
 
 
 def diff_worktree(paths=None):
     modified = []
+    deleted = set()
     submodules = set()
 
     if paths is None:
@@ -518,8 +521,10 @@ def diff_worktree(paths=None):
             submodules.add(path)
         if status in 'DAMT':
             modified.append(path)
+            if status == 'D':
+                deleted.add(path)
 
-    return modified, submodules
+    return modified, deleted, submodules
 
 
 def diff_upstream(head):

cola/models/main.py
@@ -82,6 +82,8 @@ class MainModel(Observable):
         self.untracked = []
         self.unmerged = []
         self.upstream_changed = []
+        self.staged_deleted = set()
+        self.unstaged_deleted = set()
         self.submodules = set()
 
         self.local_branches = []
@@ -192,8 +194,10 @@ class MainModel(Observable):
         self.modified = state.get('modified', [])
         self.unmerged = state.get('unmerged', [])
         self.untracked = state.get('untracked', [])
-        self.submodules = state.get('submodules', set())
         self.upstream_changed = state.get('upstream_changed', [])
+        self.staged_deleted = state.get('staged_deleted', set())
+        self.unstaged_deleted = state.get('unstaged_deleted', set())
+        self.submodules = state.get('submodules', set())
 
         sel = selection_model()
         if self.is_empty():

cola/qtutils.py
@@ -163,10 +163,10 @@ class TreeWidgetItem(QtGui.QTreeWidgetItem):
 
     TYPE = QtGui.QStandardItem.UserType + 101
 
-    def __init__(self, path, icon, exists):
+    def __init__(self, path, icon, deleted):
         QtGui.QTreeWidgetItem.__init__(self)
         self.path = path
-        self.exists = exists
+        self.deleted = deleted
         self.setIcon(0, cached_icon_from_path(icon))
         self.setText(0, path)
 
@@ -448,37 +448,19 @@ def icon_name_for_filename(filename):
     return KNOWN_FILE_EXTENSIONS.get(extension.lower(), 'generic.png')
 
 
-def icon_name_for_file(filename, staged=False, untracked=False):
-    """Returns a file path representing a corresponding file path."""
-    exists = True
-    if staged:
-        exists = core.exists(filename)
-        if exists:
-            icon_name = 'staged-item.png'
-        else:
-            icon_name = 'removed.png'
+def create_treeitem(filename, staged=False, deleted=False, untracked=False):
+    """Given a filename, return a TreeListItem suitable for adding to a
+    QListWidget.  "staged", "deleted, and "untracked" control whether to use
+    the appropriate icons."""
+    if deleted:
+        icon_name = 'removed.png'
+    elif staged:
+        icon_name = 'staged-item.png'
     elif untracked:
         icon_name = 'untracked.png'
     else:
-        exists = core.exists(filename)
-        if exists:
-            icon_name = icon_name_for_filename(filename)
-        else:
-            icon_name = 'removed.png'
-    return (icon_name, exists)
-
-
-def create_treeitem(filename, staged=False, untracked=False, check=True):
-    """Given a filename, return a QListWidgetItem suitable
-    for adding to a QListWidget.  "staged" and "untracked"
-    controls whether to use the appropriate icons."""
-    if check:
-        (icon_name, exists) = icon_name_for_file(filename, staged=staged,
-                                                 untracked=untracked)
-    else:
-        exists = True
-        icon_name = 'staged-item.png'
-    return TreeWidgetItem(filename, resources.icon(icon_name), exists=exists)
+        icon_name = icon_name_for_filename(filename)
+    return TreeWidgetItem(filename, resources.icon(icon_name), deleted=deleted)
 
 
 @memoize

commit 3e380e25ce3b39ac276d20e4176bb15f13b26d6c

widgets/status: fix unstaged deleted diff
2cbd38e2edca6ae6aa732b7afb809fb4b2a7dbf0 broke displaying the diff for
an unstaged deleted file.  In the call to cmds.do(cmds.Diff, ...),
item.deleted was being passed as a positional argument, which meant it
was actually being assigned to the cached argument, rather than the
deleted argument, resulting in an empty diff.  Change to pass as a
keyword argument to avoid this.

Signed-off-by: Daniel Harding <dharding@living180.net>

cola/widgets/status.py
@@ -840,12 +840,12 @@ class StatusTreeWidget(QtGui.QTreeWidget):
         # A staged file
         elif category == self.idx_staged:
             item = self.staged_items()[0]
-            cmds.do(cmds.DiffStaged, item.path, item.deleted)
+            cmds.do(cmds.DiffStaged, item.path, deleted=item.deleted)
 
         # A modified file
         elif category == self.idx_modified:
             item = self.modified_items()[0]
-            cmds.do(cmds.Diff, item.path, item.deleted)
+            cmds.do(cmds.Diff, item.path, deleted=item.deleted)
 
         elif category == self.idx_unmerged:
             item = self.unmerged_items()[0]