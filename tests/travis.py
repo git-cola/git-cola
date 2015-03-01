15d53d94793788ada7738615d6a5a38b065290b6
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2015-03-01 23:06:07 +0100

    Added travis.py script and defined yaml configuration

---
+++ tests/travis.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ['QT_API'] = os.environ['USE_QT_API']
+
+from pyqode.qt import QtCore, QtGui, QtWidgets
+
+print('Qt version:%s' % QtCore.__version__)
+print(QtCore.QEvent)
+print(QtGui.QPainter)
+print(QtWidgets.QWidget)
