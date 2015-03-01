e9d9b7d6076f32be52dccbc04ceb77d491e2e08b
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2015-03-01 19:38:55 +0100

    Push version and update README:

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![PyPI version](https://badge.fury.io/py/QtPy.svg)](http://badge.fury.io/py/QtPy)
+
 About
 -----
 
--- qtpy/__init__.py
+++ qtpy/__init__.py
@@ -60,7 +60,7 @@ import os
 import sys
 import logging
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 
 #: Qt API environment variable name
 QT_API = 'QT_API'
