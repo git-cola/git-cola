5f1b4e4b7ac56c8d4d2170cb4fbeeba5d42ad95f
Thomas Kluyver <takowl@gmail.com>
2015-04-24 16:49:07 -0700

    Refine files included for Windows

--- fetch_pyqt_windows.sh
+++ fetch_pyqt_windows.sh
@@ -11,7 +11,10 @@ BITNESS=32
 
 INSTALLER_FILE=PyQt4-${PYQT_VERSION}-gpl-Py${PY_VERSION}-Qt${QT_VERSION}-x${BITNESS}.exe
 URL=http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-${PYQT_VERSION}/${INSTALLER_FILE}
-#wget -O "$INSTALLER_FILE" "$URL"
+if [! -e "$INSTALLER_FILE" ]
+then
+    wget -O "$INSTALLER_FILE" "$URL"
+fi
 
 rm -rf pyqt4-windows
 mkdir pyqt4-windows
@@ -24,8 +27,6 @@ echo "Rearranging files into pynsist_pkgs..."
 mv 'pyqt4-windows/Lib/site-packages'/* pynsist_pkgs/
 rm pynsist_pkgs/PyQt4/assistant.exe pynsist_pkgs/PyQt4/designer.exe
 mv 'pyqt4-windows/$_OUTDIR/'*.pyd pynsist_pkgs/PyQt4/
-# These may not be necessary:
-mv 'pyqt4-windows/$_OUTDIR/qsci/' 'pyqt4-windows/$_OUTDIR/sip/' pynsist_pkgs/PyQt4/
 
 rm -r pyqt4-windows
 echo "Done"
