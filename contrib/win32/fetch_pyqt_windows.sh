2d9266c36865162cbe9d05a6abd59db343d7eae4
Thomas Kluyver <takowl@gmail.com>
2015-04-27 10:31:03 -0700

    Move fetch_pyqt_windows.sh to contrib/win32

--- contrib/win32/fetch_pyqt_windows.sh
+++ contrib/win32/fetch_pyqt_windows.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 PY_VERSION=3.4
 PYQT_VERSION=4.11.3
 QT_VERSION=4.8.6
@@ -11,7 +13,8 @@ BITNESS=32
 
 INSTALLER_FILE=PyQt4-${PYQT_VERSION}-gpl-Py${PY_VERSION}-Qt${QT_VERSION}-x${BITNESS}.exe
 URL=http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-${PYQT_VERSION}/${INSTALLER_FILE}
-if [! -e "$INSTALLER_FILE" ]
+
+if [ ! -e "$INSTALLER_FILE" ]
 then
     wget -O "$INSTALLER_FILE" "$URL"
 fi
@@ -20,13 +23,14 @@ rm -rf pyqt4-windows
 mkdir pyqt4-windows
 7z x -opyqt4-windows "$INSTALLER_FILE"
 
-rm -rf pynsist_pkgs
-mkdir pynsist_pkgs
+PKGS=../../pynsist_pkgs
+rm -rf $PKGS
+mkdir $PKGS
 
 echo "Rearranging files into pynsist_pkgs..."
-mv 'pyqt4-windows/Lib/site-packages'/* pynsist_pkgs/
-rm pynsist_pkgs/PyQt4/assistant.exe pynsist_pkgs/PyQt4/designer.exe
-mv 'pyqt4-windows/$_OUTDIR/'*.pyd pynsist_pkgs/PyQt4/
+mv 'pyqt4-windows/Lib/site-packages'/* $PKGS
+rm $PKGS/PyQt4/assistant.exe $PKGS/PyQt4/designer.exe
+mv 'pyqt4-windows/$_OUTDIR/'*.pyd $PKGS/PyQt4/
 
 rm -r pyqt4-windows
 echo "Done"
