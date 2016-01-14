d77de1c83adf027855d45e89ed3c093ad7cd3edd
Carlos Cordoba <ccordoba12@gmail.com>
2016-01-14 14:02:03 -0500

    QtCore: Add missing imports from Spyder shim

--- qtpy/QtCore.py
+++ qtpy/QtCore.py
@@ -11,6 +11,7 @@ Provides QtCore classes and functions.
 """
 
 import os
+
 from qtpy import QT_API
 from qtpy import PYQT5_API
 from qtpy import PYQT4_API
@@ -19,28 +20,26 @@ from qtpy import PythonQtError
 
 
 if os.environ[QT_API] in PYQT5_API:
-    from PyQt5.QtCore import *                                # analysis:ignore
-    # compatibility with pyside
+    from PyQt5.QtCore import *
     from PyQt5.QtCore import pyqtSignal as Signal
     from PyQt5.QtCore import pyqtSlot as Slot
     from PyQt5.QtCore import pyqtProperty as Property
-    # use a common __version__
     from PyQt5.QtCore import QT_VERSION_STR as __version__
 elif os.environ[QT_API] in PYQT4_API:
-    from PyQt4.QtCore import *                                # analysis:ignore
-    # compatibility with pyside
+    from PyQt4.QtCore import *
+    from PyQt4.QtCore import QCoreApplication
+    from PyQt4.QtCore import Qt
     from PyQt4.QtCore import pyqtSignal as Signal
     from PyQt4.QtCore import pyqtSlot as Slot
     from PyQt4.QtCore import pyqtProperty as Property
-    from PyQt4.QtGui import QSortFilterProxyModel
-    # use a common __version__
+    from PyQt4.QtGui import (QItemSelection, QItemSelectionRange,
+                             QSortFilterProxyModel)
     from PyQt4.QtCore import QT_VERSION_STR as __version__
 elif os.environ[QT_API] in PYSIDE_API:
-    from PySide.QtCore import *                               # analysis:ignore
-    from PySide.QtGui import QSortFilterProxyModel
-    # use a common __version__
+    from PySide.QtCore import *
+    from PySide.QtGui import (QItemSelection, QItemSelectionRange,
+                              QSortFilterProxyModel)
     import PySide.QtCore
     __version__ = PySide.QtCore.__version__
 else:
     raise PythonQtError('No Qt bindings could be found')
-
