7300a17dc7a36158eba24436b61fcd2b99259e21
Carlos Cordoba <ccordoba12@gmail.com>
2016-01-15 14:00:39 -0500

    QtGui: Only import classes compatible with PyQt5 for PyQt4 and PySide

--- qtpy/QtGui.py
+++ qtpy/QtGui.py
@@ -8,9 +8,9 @@
 
 """
 Provides QtGui classes and functions.
-.. warning:: All PyQt4/PySide gui classes are exposed but when you use
-    PyQt5, those classes are not available. Therefore, you should treat/use
-    this package as if it was ``PyQt5.QtGui`` module.
+.. warning:: Only PyQt4/PySide Gui classes compatible with PyQt5 are exposed
+    here. Therefore, you need to treat/use this package as if it were
+    the ``PyQt5.QtGui`` module.
 """
 
 import os
@@ -24,9 +24,85 @@ from qtpy import PythonQtError
 if os.environ[QT_API] in PYQT5_API:
     from PyQt5.QtGui import *                                 # analysis:ignore
 elif os.environ[QT_API] in PYQT4_API:
-    from PyQt4.QtGui import *                                 # analysis:ignore
+    from PyQt4.QtGui import (QAbstractTextDocumentLayout, QActionEvent, QBitmap,
+                             QBrush, QClipboard, QCloseEvent, QColor,
+                             QConicalGradient, QContextMenuEvent, QCursor,
+                             QDesktopServices, QDoubleValidator, QDrag,
+                             QDragEnterEvent, QDragLeaveEvent, QDragMoveEvent,
+                             QDropEvent, QFileOpenEvent, QFocusEvent, QFont,
+                             QFontDatabase, QFontInfo, QFontMetrics,
+                             QFontMetricsF, QGlyphRun, QGradient, QHelpEvent,
+                             QHideEvent, QHoverEvent, QIcon, QIconDragEvent,
+                             QIconEngine, QImage, QImageIOHandler, QImageReader,
+                             QImageWriter, QInputEvent, QInputMethodEvent,
+                             QKeyEvent, QKeySequence, QLinearGradient,
+                             QMatrix2x2, QMatrix2x3, QMatrix2x4, QMatrix3x2,
+                             QMatrix3x3, QMatrix3x4, QMatrix4x2, QMatrix4x3,
+                             QMatrix4x4, QMouseEvent, QMoveEvent, QMovie,
+                             QPaintDevice, QPaintEngine, QPaintEngineState,
+                             QPaintEvent, QPainter, QPainterPath,
+                             QPainterPathStroker, QPalette, QPen, QPicture,
+                             QPictureIO, QPixmap, QPixmapCache, QPolygon,
+                             QPolygonF, QQuaternion, QRadialGradient, QRawFont,
+                             QRegExpValidator, QRegion, QResizeEvent,
+                             QSessionManager, QShortcutEvent, QShowEvent,
+                             QStandardItem, QStandardItemModel, QStaticText,
+                             QStatusTipEvent, QSyntaxHighlighter, QTabletEvent,
+                             QTextBlock, QTextBlockFormat, QTextBlockGroup,
+                             QTextBlockUserData, QTextCharFormat, QTextCursor,
+                             QTextDocument, QTextDocumentFragment,
+                             QTextDocumentWriter, QTextFormat, QTextFragment,
+                             QTextFrame, QTextFrameFormat, QTextImageFormat,
+                             QTextInlineObject, QTextItem, QTextLayout,
+                             QTextLength, QTextLine, QTextList, QTextListFormat,
+                             QTextObject, QTextObjectInterface, QTextOption,
+                             QTextTable, QTextTableCell, QTextTableCellFormat,
+                             QTextTableFormat, QTouchEvent, QTransform,
+                             QValidator, QVector2D, QVector3D, QVector4D,
+                             QWhatsThisClickedEvent, QWheelEvent,
+                             QWindowStateChangeEvent, qAlpha, qBlue,
+                             qFuzzyCompare, qGray, qGreen, qIsGray, qRed, qRgb,
+                             qRgba)
 elif os.environ[QT_API] in PYSIDE_API:
-    from PySide.QtGui import *                                # analysis:ignore
+    from PySide.QtGui import (QAbstractTextDocumentLayout, QActionEvent, QBitmap,
+                              QBrush, QClipboard, QCloseEvent, QColor,
+                              QConicalGradient, QContextMenuEvent, QCursor,
+                              QDesktopServices, QDoubleValidator, QDrag,
+                              QDragEnterEvent, QDragLeaveEvent, QDragMoveEvent,
+                              QDropEvent, QFileOpenEvent, QFocusEvent, QFont,
+                              QFontDatabase, QFontInfo, QFontMetrics,
+                              QFontMetricsF, QGlyphRun, QGradient, QHelpEvent,
+                              QHideEvent, QHoverEvent, QIcon, QIconDragEvent,
+                              QIconEngine, QImage, QImageIOHandler, QImageReader,
+                              QImageWriter, QInputEvent, QInputMethodEvent,
+                              QKeyEvent, QKeySequence, QLinearGradient,
+                              QMatrix2x2, QMatrix2x3, QMatrix2x4, QMatrix3x2,
+                              QMatrix3x3, QMatrix3x4, QMatrix4x2, QMatrix4x3,
+                              QMatrix4x4, QMouseEvent, QMoveEvent, QMovie,
+                              QPaintDevice, QPaintEngine, QPaintEngineState,
+                              QPaintEvent, QPainter, QPainterPath,
+                              QPainterPathStroker, QPalette, QPen, QPicture,
+                              QPictureIO, QPixmap, QPixmapCache, QPolygon,
+                              QPolygonF, QQuaternion, QRadialGradient, QRawFont,
+                              QRegExpValidator, QRegion, QResizeEvent,
+                              QSessionManager, QShortcutEvent, QShowEvent,
+                              QStandardItem, QStandardItemModel, QStaticText,
+                              QStatusTipEvent, QSyntaxHighlighter, QTabletEvent,
+                              QTextBlock, QTextBlockFormat, QTextBlockGroup,
+                              QTextBlockUserData, QTextCharFormat, QTextCursor,
+                              QTextDocument, QTextDocumentFragment,
+                              QTextDocumentWriter, QTextFormat, QTextFragment,
+                              QTextFrame, QTextFrameFormat, QTextImageFormat,
+                              QTextInlineObject, QTextItem, QTextLayout,
+                              QTextLength, QTextLine, QTextList, QTextListFormat,
+                              QTextObject, QTextObjectInterface, QTextOption,
+                              QTextTable, QTextTableCell, QTextTableCellFormat,
+                              QTextTableFormat, QTouchEvent, QTransform,
+                              QValidator, QVector2D, QVector3D, QVector4D,
+                              QWhatsThisClickedEvent, QWheelEvent,
+                              QWindowStateChangeEvent, qAlpha, qBlue,
+                              qFuzzyCompare, qGray, qGreen, qIsGray, qRed, qRgb,
+                              qRgba)
 else:
     raise PythonQtError('No Qt bindings could be found')
 
