6edab209abbfbbca88f50f03f8bcabad460b5464
Carlos Cordoba <ccordoba12@gmail.com>
2016-01-16 13:17:39 -0500

    Setup.py: Add 3.5 to the list of supported Python versions

--- setup.py
+++ setup.py
@@ -85,5 +85,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Topic :: Software Development :: Widget Sets'])
-
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Widget Sets']
+)
