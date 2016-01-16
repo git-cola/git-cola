7e96cc8d0ad29c9de7b84ff866ff2450c20f6d18
David Aguilar <davvid@gmail.com>
2016-01-16 01:50:42 -0800

    github.py: use requests.Session instead of manually managing headers
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- github.py
+++ github.py
@@ -9,33 +9,32 @@ class GithubAPI(object):
     CONFIG = os.path.join(os.path.dirname(__file__), 'config')
 
     def __init__(self):
-        self.headers = {
-                'Accept': 'application/vnd.github.manifold-preview',
-        }
+        self.session = session = requests.Session()
+        headers = session.headers
         with open(self.CONFIG, 'r') as f:
             for line in f:
                 if line.startswith('GITHUB_TOKEN='):
                     key, value = line.split('=', 1)
-                    self.headers['Authorization'] = 'token %s' % value.strip()
+                    headers['Authorization'] = 'token %s' % value.strip()
 
     def get(self, path):
         url = self.BASE_URL + path
-        return requests.get(url, headers=self.headers)
+        return self.session.get(url)
 
     def post(self, path, **kwargs):
         url = self.BASE_URL + path
-        return requests.post(url, headers=self.headers, **kwargs)
+        return self.session.post(url, **kwargs)
 
     def patch(self, path, **kwargs):
         url = self.BASE_URL + path
-        return requests.patch(url, headers=self.headers, **kwargs)
+        return self.session.patch(url, **kwargs)
 
     def delete(self, path, **kwargs):
         url = self.BASE_URL + path
-        return requests.delete(url, headers=self.headers, **kwargs)
+        return self.session.delete(url, **kwargs)
 
     def upload_headers(self, filename):
         mimetype = mimetypes.guess_type(filename)[0]
-        headers = ['%s: %s' % (k, v) for k, v in self.headers.items()]
+        headers = ['%s: %s' % (k, v) for k, v in self.session.headers.items()]
         headers.append('Content-Type: %s' % mimetype)
         return headers
