33f2b38e2a9a18f12eb3efd5e5f30723848d4a08
David Aguilar <davvid@gmail.com>
2016-01-16 13:25:43 -0800

    test: remove unneeded setup/teardown with QApplication
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/app_test.py
+++ test/app_test.py
@@ -1,42 +1,25 @@
 #!/usr/bin/env python
+
+from __future__ import absolute_import, division, unicode_literals
+
+import argparse
 import unittest
 
 from cola import app
-
-from PyQt4 import QtGui
 
 from test.helper import run_unittest
 
 
 class AppTestCase(unittest.TestCase):
 
-    class MockQtGuiQApplication(object):
-        def __init__(self, *args):
-            pass
-
-    class Mock_Session_Mgr(object):
-        def sessionId(self):
-            return 'junk_string'
-
-        def sessionKey(self):
-            return 'junk_string'
-
-    class MockView(object):
-        def save_state(self, *args, **kwargs):
-            pass
-
-    def setUp(self):
-        super(AppTestCase, self).setUp()
-        self.Store_QApplication = QtGui.QApplication
-        QtGui.QApplication = self.MockQtGuiQApplication
-
-    def tearDown(self):
-        super(AppTestCase, self).tearDown()
-        QtGui.QApplication = self.Store_QApplication
-
     def test_setup_environment(self):
-        #If the function doesn't throw an exception we are happy.
+        # If the function doesn't throw an exception we are happy.
         app.setup_environment()
+
+    def test_add_common_arguments(self):
+        # If the function doesn't throw an exception we are happy.
+        parser = argparse.ArgumentParser()
+        app.add_common_arguments(parser)
 
 
 def test_suite():
