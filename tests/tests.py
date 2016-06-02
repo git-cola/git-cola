5e952df2776661cd3b054754d17fc16962d00844
Carlos Cordoba <ccordoba12@gmail.com>
2016-06-01 21:55:10 -0500

    Eliminate unneeded formatting rules

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,7 @@ os.environ['QT_API'] = os.environ['USE_QT_API'].lower()
 
 from qtpy import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 
-print('Qt version:{0!s}'.format(QtCore.__version__))
+print('Qt version:{0}'.format(QtCore.__version__))
 print(QtCore.QEvent)
 print(QtGui.QPainter)
 print(QtWidgets.QWidget)
