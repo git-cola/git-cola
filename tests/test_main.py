6a9fb4f8ad3c8fda2b12688be5058e95d5e995e7
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-06-09 11:01:09 +0100

    Check that the priority order is respected if QT_API or USE_QT_API are not specified.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,32 +1,57 @@
 import os
 
+from qtpy import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
+
+
+def assert_pyside():
+    import PySide
+    assert QtCore.QEvent is PySide.QtCore.QEvent
+    assert QtGui.QPainter is PySide.QtGui.QPainter
+    assert QtWidgets.QWidget is PySide.QtGui.QWidget
+    assert QtWebEngineWidgets.QWebEnginePage is PySide.QtWebKit.QWebPage
+
+
+def assert_pyqt4():
+    import PyQt4
+    assert QtCore.QEvent is PyQt4.QtCore.QEvent
+    assert QtGui.QPainter is PyQt4.QtGui.QPainter
+    assert QtWidgets.QWidget is PyQt4.QtGui.QWidget
+    assert QtWebEngineWidgets.QWebEnginePage is PyQt4.QtWebKit.QWebPage
+
+
+def assert_pyqt5():
+    import PyQt5
+    assert QtCore.QEvent is PyQt5.QtCore.QEvent
+    assert QtGui.QPainter is PyQt5.QtGui.QPainter
+    assert QtWidgets.QWidget is PyQt5.QtWidgets.QWidget
+    assert QtWebEngineWidgets.QWebEnginePage is PyQt5.QtWebEngineWidgets.QWebEnginePage
+
 
 def test_qt_api():
     """
     If QT_API is specified, we check that the correct Qt wrapper was used
     """
 
-    from qtpy import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
-
     QT_API = os.environ.get('QT_API', None)
 
     if QT_API == 'pyside':
-        import PySide
-        assert QtCore.QEvent is PySide.QtCore.QEvent
-        assert QtGui.QPainter is PySide.QtGui.QPainter
-        assert QtWidgets.QWidget is PySide.QtGui.QWidget
-        assert QtWebEngineWidgets.QWebEnginePage is PySide.QtWebKit.QWebPage
+        assert_pyside()
     elif QT_API in ('pyqt', 'pyqt4'):
-        import PyQt4
-        assert QtCore.QEvent is PyQt4.QtCore.QEvent
-        assert QtGui.QPainter is PyQt4.QtGui.QPainter
-        assert QtWidgets.QWidget is PyQt4.QtGui.QWidget
-        assert QtWebEngineWidgets.QWebEnginePage is PyQt4.QtWebKit.QWebPage
+        assert_pyqt4()
     elif QT_API == 'pyqt5':
-        import PyQt5
-        assert QtCore.QEvent is PyQt5.QtCore.QEvent
-        assert QtGui.QPainter is PyQt5.QtGui.QPainter
-        assert QtWidgets.QWidget is PyQt5.QtWidgets.QWidget
-        assert QtWebEngineWidgets.QWebEnginePage is PyQt5.QtWebEngineWidgets.QWebEnginePage
+        assert_pyqt5()
     else:
-        pass
+        # We should then be loading, in order of decreasing preference, PyQt5,
+        # PyQt4, and PySide.
+        try:
+            import PyQt5
+        except ImportError:
+            try:
+                import PyQt4
+            except ImportError:
+                import PySide
+                assert_pyside()
+            else:
+                assert_pyqt4()
+        else:
+            assert_pyqt5()
