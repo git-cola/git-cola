3a4cf0528b9ed987196df97391a3f77bb63bdaca
David Aguilar <davvid@gmail.com>
2016-06-23 00:39:56 -0700

    interaction: apply flake8 suggestions
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/interaction.py
+++ cola/interaction.py
@@ -22,8 +22,8 @@ class Interaction(object):
         scope['title_dashes'] = '-' * len(title)
         scope['message'] = message
         scope['details'] = details and '\n'+details or ''
-        scope['informative_text'] = (informative_text and
-                '\n'+informative_text or '')
+        scope['informative_text'] = (
+                informative_text and ('\n' + informative_text) or '')
         sys.stdout.write("""
 %(title)s
 %(title_dashes)s
