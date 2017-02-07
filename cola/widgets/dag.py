5c2f1c430d568c78f41a04aee8019a7d873eef9e
David Aguilar <davvid@gmail.com>
2017-02-06 19:55:44 -0800

    Revert "dag: skip saving painter state when painting"
    This reverts commit ad94c330afdb271286cddd03079b327cb8cb087d.
    
    Labels disappear when the painter state is not saved.
    
    Reported-by: Efimov Vasily <real@ispras.ru>
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/dag.py
+++ cola/widgets/dag.py
@@ -1206,7 +1206,6 @@ class GraphView(QtWidgets.QGraphicsView, ViewerMixin):
         self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
         self.setResizeAnchor(QtWidgets.QGraphicsView.NoAnchor)
         self.setBackgroundBrush(QtGui.QColor(Qt.white))
-        self.setOptimizationFlags(QtWidgets.QGraphicsView.DontSavePainterState)
 
         qtutils.add_action(self, N_('Zoom In'), self.zoom_in,
                            hotkeys.ZOOM_IN, hotkeys.ZOOM_IN_SECONDARY)
