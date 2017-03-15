ac23b8d7c61645bbc90c28d7c98bdb9de8ad54b1
David Aguilar <davvid@gmail.com>
2017-03-15 01:13:00 -0700

    about: handle qtpy.PythonQtError
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/about.py
+++ cola/widgets/about.py
@@ -376,7 +376,7 @@ def show_shortcuts():
     hotkeys_html = resources.doc(N_('hotkeys.html'))
     try:
         from qtpy import QtWebEngineWidgets
-    except ImportError:
+    except (ImportError, qtpy.PythonQtError):
         # redhat disabled QtWebKit in their qt build but don't punish the users
         webbrowser.open_new_tab('file://' + hotkeys_html)
         return
