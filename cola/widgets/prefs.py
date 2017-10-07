f05ee9cd5ba610501476e456654c8a19553b106d
David Aguilar <davvid@gmail.com>
2017-10-07 15:16:34 -0700

    prefs: move the "Close" button to the right
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/prefs.py
+++ cola/widgets/prefs.py
@@ -265,7 +265,7 @@ class PreferencesView(standard.Dialog):
         self.close_button = qtutils.close_button()
 
         self.button_layout = qtutils.hbox(defs.no_margin, defs.spacing,
-                                          self.close_button, qtutils.STRETCH)
+                                          qtutils.STRETCH, self.close_button)
 
         self.main_layout = qtutils.vbox(defs.margin, defs.spacing,
                                         self.tab_bar, self.stack_widget,
