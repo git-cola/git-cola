86c1a0eaa25a88313d83a9f867d06ff811f052f2
David Aguilar <davvid@gmail.com>
2018-03-05 21:01:43 -0800

    imageview: add mainloop bits from qimageview/viewer.py
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/imageview.py
+++ cola/widgets/imageview.py
@@ -44,6 +44,17 @@ except ImportError:
 def clamp(a, _min, _max):
     return max(min(a, _max), _min)
 
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QDesktopWidget, QLineEdit, QSlider
+from PyQt5.QtGui import QImage
+from PyQt5.QtCore import QPoint, QPointF, QRect, QRectF, QSize, QSizeF, pyqtSignal
+from PyQt5.Qt import Qt
+from enum import Enum
+import os, sys
+
+main_loop_type = 'qt'
+
+
 class ImageView(QGraphicsView):
     imageChanged = pyqtSignal()
 
@@ -296,3 +307,124 @@ class ImageView(QGraphicsView):
             xratio = yratio = max(xratio, yratio)
         self.scale(xratio, yratio)
         self.centerOn(rect.center())
+
+
+class AppImageView(ImageView):
+    def __init__(self, *args, **kwargs):
+        ImageView.__init__(self, *args, **kwargs)
+        scene = self.scene()
+        self.main_widget = None
+
+    def mousePressEvent(self, event):
+        ImageView.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        ImageView.mouseMoveEvent(self, event)
+        pos = event.pos()
+        scene_pos = self.mapToScene(pos)
+        msg = 'ui: %d, %d  image: %d, %d'%(pos.y(), pos.x(), round(scene_pos.y()), round(scene_pos.x()))
+        self.main_widget.statusBar().showMessage(msg)
+
+
+class ImageViewerWindow(QMainWindow):
+    def __init__(self, image, input_path):
+        QMainWindow.__init__(self)
+        self.image = image
+        self.input_path = input_path
+        self.image_view = AppImageView(self)
+        self.image_view.main_widget = self
+        self.statusBar().showMessage("")
+
+        #self.resize(image.size())
+        padding = self.frameGeometry().size() - self.geometry().size()
+        self.resize(image.size() + padding)
+        #self.resize(797, 615)
+
+        central = QWidget(self)
+        central.setObjectName("MainWindow")
+
+        self.verticalLayout = QtWidgets.QVBoxLayout(central)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        #self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
+        self.verticalLayout.setObjectName("verticalLayout")
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.image_view.sizePolicy().hasHeightForWidth())
+        self.image_view.setSizePolicy(sizePolicy)
+        self.image_view.setMouseTracking(True)
+        self.image_view.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.image_view.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.image_view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.image_view.setObjectName("image_view")
+        self.verticalLayout.addWidget(self.image_view)
+
+        self.setCentralWidget(central)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+        screen = QDesktopWidget().screenGeometry(self)
+        size = self.geometry()
+        self.move((screen.width()-size.width())/4, (screen.height()-size.height())/4)
+
+        self.update_view()
+        self.image_view.reset()
+
+    def hideEvent(self, event):
+        QMainWindow.hide(self)
+
+    def update_view(self):
+        self.image_view.image = self.image
+        self.setWindowTitle(self.make_window_title())
+
+    def make_window_title(self):
+        return os.path.basename(self.input_path)
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        modifier = event.modifiers()
+        global main_loop_type
+        if key == Qt.Key_Escape:
+            if main_loop_type == 'qt':
+                QApplication.quit()
+            elif main_loop_type == 'ipython':
+                self.hide()
+                #import IPython
+                #IPython.get_ipython().ask_exit()
+
+
+def sigint_handler(*args):
+    """Handler for the SIGINT signal."""
+    sys.stderr.write('\r')
+    QApplication.quit()
+
+
+def main():
+    import argparse, errno, sys
+    parser = argparse.ArgumentParser(description='image viewer')
+    parser.add_argument('inputs', type=str, nargs=1, help='path to the image')
+    parser.add_argument('--interactive', '-i', action='store_true', help='launch in interactive shell')
+    opts = parser.parse_args()
+
+    input_image = opts.inputs[0]
+    image = QImage()
+    image.load(input_image)
+
+    app = QApplication(sys.argv)
+    try:
+        import signal
+        signal.signal(signal.SIGINT, sigint_handler)
+    except ImportError:
+        pass
+    window = ImageViewerWindow(image, input_image)
+    window.show()
+
+    if opts.interactive:
+        global main_loop_type
+        main_loop_type = 'ipython'
+        from IPython import start_ipython
+        start_ipython(user_ns=dict(globals(), **locals()), argv=[])
+    else:
+        app.exec_()
+
+if __name__ == '__main__':
+    main()
