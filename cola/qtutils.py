edd32620671a15ae23ce4464be417b173dade018
David Aguilar <davvid@gmail.com>
2018-03-06 01:00:57 -0800

    qtutils: add an ImageFormats class
    Quickly query whether a filename is a supported image format.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/qtutils.py
+++ cola/qtutils.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2007-2018 David Aguilar and contributors
 """Miscellaneous Qt utility functions."""
 from __future__ import division, absolute_import, unicode_literals
+import os
 
 from qtpy import compat
 from qtpy import QtGui
@@ -941,3 +942,18 @@ def make_format(fg=None, bg=None, bold=False):
     if bold:
         fmt.setFontWeight(QtGui.QFont.Bold)
     return fmt
+
+
+class ImageFormats(object):
+
+    def __init__(self):
+        # returns a list of QByteArray objects
+        formats_qba = QtGui.QImageReader.supportedImageFormats()
+        # portability: python3 data() returns bytes, python2 returns str
+        decode = core.decode
+        formats = [decode(x.data()) for x in formats_qba]
+        self.extensions = set(['.' + fmt for fmt in formats])
+
+    def ok(self, filename):
+        _, ext = os.path.splitext(filename)
+        return ext in self.extensions
