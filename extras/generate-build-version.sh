3446e6c2d749cf0954eb55a83c6248688fe7e30d
David Aguilar <davvid@gmail.com>
2018-08-04 18:49:06 -0700

    generate-build.sh: pylint updates
    Add a module docstring to the generated version module.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- extras/generate-build-version.sh
+++ extras/generate-build-version.sh
@@ -5,7 +5,8 @@ GIT=${GIT:-git}
 if "$GIT" version 2>&1 >/dev/null && test -e .git
 then
     BUILD_VERSION=$("$GIT" describe --first-parent) &&
-    echo "BUILD_VERSION = '$BUILD_VERSION'" >cola/_build_version.py
+    echo '"""Generated build version"""' >cola/_build_version.py
+    echo "BUILD_VERSION = '$BUILD_VERSION'" >>cola/_build_version.py
 fi
 
 exit 0
