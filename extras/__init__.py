f47e05b20a81b4afcf1f356580ddda56b6d80a9d
cclauss <cclauss@bluewin.ch>
2018-08-30 11:20:22 +0200

    Fix whitespace in extras/__init__.py to placate flake8

--- extras/__init__.py
+++ extras/__init__.py
@@ -1,4 +1,4 @@
-from __future__  import absolute_import, division, unicode_literals
+from __future__ import absolute_import, division, unicode_literals
 
 from extras.build_mo import build_mo
 from extras.build_pot import build_pot
