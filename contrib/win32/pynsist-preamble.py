b8b008e72cd0514f0e36cdc3c52f6b7e37568454
David Aguilar <davvid@gmail.com>
2018-08-31 00:59:51 -0700

    pynsist-preamble.py: improve code style
    Suggested-by: @cclauss
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- contrib/win32/pynsist-preamble.py
+++ contrib/win32/pynsist-preamble.py
@@ -1,11 +1,9 @@
 # TODO pynsist commands call it "installdir", entry points call it "scriptdir"
 import os
 try:
-    installdir = scriptdir  # noqa
+    installdir = scriptdir
 except NameError:
     pass
 pythondir = os.path.join(installdir, 'Python')
-os.environ['PATH'] = (
-    pythondir + os.pathsep
-    + pkgdir  # noqa
-    + os.pathsep + os.environ.get('PATH', ''))
+path = os.environ.get('PATH', '')
+os.environ['PATH'] = os.pathsep.join([pythondir, pkgdir, path])  # noqa
