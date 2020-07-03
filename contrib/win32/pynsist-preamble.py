c5d6bb7c2af3f8bf703c64f05395bd47ee9b3415
Thomas Kluyver <thomas@kluyver.me.uk>
2020-07-03 09:14:48 +0100

    Remove workaround no longer needed in pynsist preamble

--- contrib/win32/pynsist-preamble.py
+++ contrib/win32/pynsist-preamble.py
@@ -1,10 +1,5 @@
-# TODO pynsist commands call it "installdir", entry points call it "scriptdir"
 import os
 
-try:
-    installdir = scriptdir
-except NameError:
-    pass
 pythondir = os.path.join(installdir, 'Python')
 path = os.environ.get('PATH', '')
 os.environ['PATH'] = os.pathsep.join([pythondir, pkgdir, path])  # noqa
