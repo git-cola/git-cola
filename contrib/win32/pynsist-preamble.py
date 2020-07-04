8c9efe18d8085db0409fc4eff65b0fdbde369170
David Aguilar <davvid@gmail.com>
2020-07-04 15:43:08 -0700

    pynsist-preamble: suppres flake8 warnings
    installdir is provided by pynsist.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- contrib/win32/pynsist-preamble.py
+++ contrib/win32/pynsist-preamble.py
@@ -1,5 +1,5 @@
 import os
 
-pythondir = os.path.join(installdir, 'Python')
+pythondir = os.path.join(installdir, 'Python')  # noqa
 path = os.environ.get('PATH', '')
 os.environ['PATH'] = os.pathsep.join([pythondir, pkgdir, path])  # noqa
