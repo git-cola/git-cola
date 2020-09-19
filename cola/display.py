031ee29b0efe6e9bdaed08fa34c62f8a0ced04b1
David Aguilar <davvid@gmail.com>
2020-09-19 00:30:04 -0700

    display: add a function to create unique path suffixes
    Related-to: #1087
    Signed-off-by: David Aguilar <davvid@gmail.com>

---
+++ cola/display.py
@@ -0,0 +1,51 @@
+"""Display models and utility functions"""
+from __future__ import division, absolute_import, unicode_literals
+import collections
+import os
+
+
+def shorten_paths(paths):
+    """Shorten a sequence of paths into unique strings for display"""
+    result = {}
+    # Start by assuming that all paths are in conflict.
+    # On each iteration we will collect all the path suffixes, move the newly
+    # unique entries to the result, and repeat until no conflicts remain.
+    count = 0
+    conflicts = list(paths)
+    in_conflict = True
+    while in_conflict:
+        count += 1
+        # Gather the suffixes for the current paths in conflict
+        suffixes = collections.defaultdict(list)
+        for path in conflicts:
+            suffix = path_suffix(path, count)
+            suffixes[suffix].append(path)
+
+        # Loop over the suffixes to gather new conflicts and unique entries.
+        conflicts = []
+        in_conflict = False
+
+        for suffix, paths in suffixes.items():
+            # If only a single path exists for the suffix then no conflict
+            # exists, and the suffix is valid.
+            if len(paths) == 1:
+                result[paths[0]] = suffix
+            # If this loop runs too long then bail out by using the full path.
+            elif count >= 128:
+                for path in paths:
+                    result[path] = path
+            # If multiple paths map to the same suffix then the paths are
+            # considered in conflict, and will be reprocessed.
+            else:
+                conflicts.extend(paths)
+                in_conflict = True
+
+    return result
+
+
+def path_suffix(path, count):
+    """Return `count` number of trailing path components"""
+    components = path.split(os.sep)[-count:]
+    return os.sep.join(components)
+
+
---
+++ test/display_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, unicode_literals
+
+import pytest
+
+from cola import display
+
+
+def test_shorten_paths():
+    paths = (
+        '/usr/src/git-cola/src',
+        '/usr/src/example/src',
+        '/usr/src/super/lib/src',
+        '/usr/src/super/tools/src',
+        '/usr/src/super/example/src',
+        '/lib/src',
+    )
+    actual = display.shorten_paths(paths)
+    assert actual[paths[0]] == 'git-cola/src'
+    assert actual[paths[1]] == 'src/example/src'
+    assert actual[paths[2]] == 'super/lib/src'
+    assert actual[paths[3]] == 'tools/src'
+    assert actual[paths[4]] == 'super/example/src'
+    assert actual[paths[5]] == '/lib/src'
+
+
+if __name__ == '__main__':
+	pytest.main([__file__])
