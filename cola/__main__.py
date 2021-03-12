7dd58874bdf644e2f956d26eef4bdae5c431c4f2
Floris Lambrechts <florisla@gmail.com>
2021-03-12 22:32:31 +0100

    Allow cola to be started as a module
    This allows 'python -m cola'.

---
+++ cola/__main__.py
@@ -0,0 +1,19 @@
+"""
+Run cola as a Python module.
+
+Usage:
+
+    python -m cola
+
+"""
+
+from cola import main
+
+
+def run():
+    """Start the command-line interface."""
+    main.main()
+
+
+if __name__ == "__main__":
+    run()
