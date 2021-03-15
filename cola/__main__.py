d77ca2404fb24844a9ed37d60c765bb8f8708c94
Floris Lambrechts <florisla@gmail.com>
2021-03-15 08:32:13 +0100

    Also allow to run 'dag' from __main__.py
    This allows both entry points 'cola' and 'dag'
    to use the same Python file.

--- cola/__main__.py
+++ cola/__main__.py
@@ -8,12 +8,19 @@ Usage:
 """
 from __future__ import absolute_import, division, unicode_literals
 
+import sys
+
 from cola import main
 
 
 def run():
     """Start the command-line interface."""
     main.main()
+
+
+def run_dag():
+    args = ["dag"] + sys.argv[1:]
+    main.main(args)
 
 
 if __name__ == '__main__':
