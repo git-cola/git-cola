e5aa7a1d4fed524cc3e457f0bf0637c98712c641
Daniel Harding <dharding@living180.net>
2022-01-26 13:21:21 +0200

    stash: remove Observable usage
    StashModel never issues any notifications, so it does not need to
    inherit from Observable.
    
    Signed-off-by: Daniel Harding <dharding@living180.net>

--- cola/models/stash.py
+++ cola/models/stash.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from .. import cmds
 from .. import core
-from .. import observable
 from .. import gitcmds
 from .. import utils
 from ..i18n import N_
@@ -10,9 +9,8 @@ from ..git import STDOUT
 from ..interaction import Interaction
 
 
-class StashModel(observable.Observable):
+class StashModel(object):
     def __init__(self, context):
-        observable.Observable.__init__(self)
         self.context = context
         self.git = context.git
         self.model = model = context.model
