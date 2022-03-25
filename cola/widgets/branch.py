git-cola/git-cola

commit cc1b176e115fac6ccec4ae504e37108e8d99bde5

main: convert refs_updated notification to signal
Signed-off-by: Daniel Harding <dharding@living180.net>

cola/widgets/branch.py
@@ -53,8 +53,6 @@ class AsyncGitActionTask(qtutils.Task):
 
 
 class BranchesWidget(QtWidgets.QFrame):
-    updated = Signal()
-
     def __init__(self, context, parent):
         QtWidgets.QFrame.__init__(self, parent)
         self.model = model = context.model
@@ -96,8 +94,7 @@ class BranchesWidget(QtWidgets.QFrame):
             self.sort_order_button, cmds.run(cmds.CycleReferenceSort, context)
         )
 
-        self.updated.connect(self.refresh, Qt.QueuedConnection)
-        model.add_observer(model.message_refs_updated, self.updated.emit)
+        model.refs_updated.connect(self.refresh, Qt.QueuedConnection)
 
     def toggle_filter(self):
         shown = not self.filter_widget.isVisible()

cola/models/main.py
@@ -37,7 +37,7 @@ class MainModel(Observable):
     images_changed = Signal(object)
     mode_changed = Signal(str)
     submodules_changed = Signal()
-    message_refs_updated = 'message_refs_updated'
+    refs_updated = Signal()
     message_updated = 'updated'
     message_worktree_changed = 'message_worktree_changed'
 
@@ -319,7 +319,7 @@ class MainModel(Observable):
         self.tags = tags
         # Set these early since they are used to calculate 'upstream_changed'.
         self.currentbranch = gitcmds.current_branch(self.context)
-        self.notify_observers(self.message_refs_updated)
+        self.refs_updated.emit()
 
     def _update_merge_rebase_status(self):
         merge_head = self.git.git_path('MERGE_HEAD')

cola/widgets/completion.py
@@ -560,8 +560,7 @@ class GitRefCompletionModel(GitCompletionModel):
 
     def __init__(self, context, parent):
         GitCompletionModel.__init__(self, context, parent)
-        model = context.model
-        model.add_observer(model.message_refs_updated, self.emit_model_updated)
+        context.model.refs_updated.connect(self.model_updated)
 
     def matches(self):
         model = self.context.model

commit 2a82692fb804a1e3a87ac5bfddd74423436ad42a

branch: fix incorrect use of AsyncGitActionTask
cc1b176e115fac6ccec4ae504e37108e8d99bde5 accidentally dropped the
git_helper argument and passed in the widget itself.

Fix the mismatched arguments.

Closes #1219
Signed-off-by: David Aguilar <davvid@gmail.com>

cola/widgets/branch.py
@@ -389,7 +389,7 @@ class BranchesTreeWidget(standard.TreeWidget):
     def git_action_async(self, action, args, kwarg=None):
         if kwarg is None:
             kwarg = {}
-        task = AsyncGitActionTask(self, action, args, kwarg)
+        task = AsyncGitActionTask(self.git_helper, action, args, kwarg)
         progress = standard.progress(
             N_('Executing action %s') % action, N_('Updating'), self
         )