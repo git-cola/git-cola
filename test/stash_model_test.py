f69761475a92466cb4046db253911d7eab092cc7
David Aguilar <davvid@gmail.com>
2022-10-20 12:52:08 -0700

    tests: update stash model tests for older Git versions
    Some versions of Git do not display the expected branch name
    in the stash message. Adjust the tests to account for this.
    
    Related-to: #1267
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/stash_model_test.py
+++ test/stash_model_test.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 # pylint: disable=redefined-outer-name
 from cola.models.stash import StashModel
 
@@ -21,8 +22,9 @@ def test_stash_info_for_message_with_slash(app_context):
     helper.commit_files()
     helper.write_file('A', 'change')
     helper.run_git('stash', 'save', 'some message/something')
-    assert StashModel(app_context).stash_info()[0] \
-        == [r'stash@{0}: On main: some message/something']
+    model = StashModel(app_context)
+    stash_details = model.stash_info()[0]
+    assert stash_details == [r'stash@{0}: On main: some message/something']
 
 
 def test_stash_info_on_branch_with_slash(app_context):
@@ -30,5 +32,22 @@ def test_stash_info_on_branch_with_slash(app_context):
     helper.run_git('checkout', '-b', 'feature/a')
     helper.write_file('A', 'change')
     helper.run_git('stash', 'save', 'some message')
-    assert StashModel(app_context).stash_info()[0] \
-        == [r'stash@{0}: On feature/a: some message']
+
+    model = StashModel(app_context)
+    stash_info = model.stash_info()
+
+    stash_details = stash_info[0][0]
+    assert stash_details in (
+        'stash@{0}: On feature/a: some message',
+        # Some versions of Git do not report the full branch name
+        'stash@{0}: On a: some message',
+    )
+
+    stash_rev = stash_info[1][0]
+    assert stash_rev == r'stash@{0}'
+
+    stash_message = stash_info[3][0]
+    assert stash_message in (
+        'On feature/a: some message',
+        'On a: some message',
+    )
