8af1ff97750ae8be1052fbb28a179810b18e97e4
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2023-02-17 10:27:27 +0530

    Update qtutils.py

--- cola/qtutils.py
+++ cola/qtutils.py
@@ -464,8 +464,7 @@ def open_files(title, directory=None, filters=''):
     )
     return result[0]
 
-
-def opendir_dialog(caption, path):
+def opendir_dialog(path, caption='Browse...'):
     """Prompts for a directory path"""
     options = (
         QtWidgets.QFileDialog.Directory
