d6f466f1214d3ac4229fbdfe874761f4c2553b11
David Aguilar <davvid@gmail.com>
2023-11-02 00:14:54 -0700

    remotemessage: simplify implementation by leveraging VimTextEdit
    VimTextEdit already allows opening links using right-click actions. We
    can drop all of the regexes and url escaping by leveraging the
    pre-existing class.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/remotemessage.py
+++ cola/widgets/remotemessage.py
@@ -1,71 +1,35 @@
-from __future__ import division, absolute_import, unicode_literals
-import re
-
 from qtpy.QtCore import Qt
 
 from .. import qtutils
 from ..i18n import N_
 from . import defs
 from . import standard
-
-
-URL_REGEX = re.compile(
-    'https?://'  # Protocol
-    '([^/]+(:[^/]+)?@)?'  # Optional authentication
-    '[\\w.-]+'  # Domain name
-    '(:[0-9]+)?'  # Optional port
-    '(/[^\\s]*)?'
-)  # Optional path
-
-
-def to_link(url):
-    return qtutils.link(url, url)
-
-
-def escape(string):
-    """Escapes all occurrences of '<' and '>'"""
-    return string.replace('<', '&lt;').replace('>', '&gt;')
-
-
-def format_raw(string, start, end, offset=0):
-    """Replace a part of a string and transform it to a link"""
-    chars = list(string)
-    chars[start + offset : end + offset] = to_link(
-        string[start + offset : end + offset]
-    )
-    result = ''.join(chars)
-    return result, offset + len(result) - len(string)
-
-
-def format_links(string):
-    """Transform all links into HTML links"""
-    offset = 0
-    for url_match in URL_REGEX.finditer(string):
-        string, offset = format_raw(string, url_match.start(), url_match.end(), offset)
-    return string
+from . import text
 
 
 def show(context, message):
     """Display a window if the remote sent a message"""
-    message_lines = escape(message).split('\n')
-    # Get lines starting with 'remote: '
-    remote_lines = [line[8:] for line in message_lines if line.startswith('remote: ')]
-    if remote_lines:
-        # Transform new lines to HTML '<br>' and render all white spaces
-        remote_message = (
-            '<body style="white-space: pre">' + '<br>'.join(remote_lines) + '</body>'
-        )
-        view = RemoteMessage(context, format_links(remote_message), parent=context.view)
+    if message:
+        view = RemoteMessage(context, message, parent=context.view)
         view.show()
+        view.exec_()
 
 
 def with_context(context):
-    """Helper function to pass to the `result` argument of RunTask.start"""
+    """Return a closure for the `result` callback from RunTask.start()"""
 
-    def wrapper(raw_message):
-        return show(context, raw_message[2])
+    def show_result(result):
+        """Display the asynchronous "result" when remote tasks complete"""
+        _, out, err = result
+        output = '\n\n'.join((x for x in (out, err) if x))
+        if output:
+            message = N_('Right-click links to open:') + '\n\n' + output
+        else:
+            message = output
 
-    return wrapper
+        return show(context, message)
+
+    return show_result
 
 
 class RemoteMessage(standard.Dialog):
@@ -76,19 +40,24 @@ class RemoteMessage(standard.Dialog):
         self.context = context
         self.model = context.model
 
-        self.setWindowTitle(N_('Remote message'))
+        self.setWindowTitle(N_('Remote Messages'))
         if parent is not None:
             self.setWindowModality(Qt.WindowModal)
 
-        self.text = qtutils.textbrowser(text=message)
+        self.text = text.VimTextEdit(context, parent=self)
+        self.text.set_value(message)
         # Set a monospace font, as some remote git messages include ASCII art
         self.text.setFont(qtutils.default_monospace_font())
 
         self.close_button = qtutils.close_button()
         self.close_button.setDefault(True)
 
+        self.bottom_layout = qtutils.hbox(
+            defs.no_margin, defs.button_spacing, qtutils.STRETCH, self.close_button
+        )
+
         self.main_layout = qtutils.vbox(
-            defs.no_margin, defs.spacing, self.text, self.close_button
+            defs.no_margin, defs.spacing, self.text, self.bottom_layout
         )
         self.setLayout(self.main_layout)
 
