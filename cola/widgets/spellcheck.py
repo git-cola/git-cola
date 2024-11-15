git-cola/git-cola

commit e7425622c3105109e0f489c4f849b24c1a11c1ed

spellcheck: resize the SpellCheckLineEdit when the font is applied
Resize the spellcheck lineedit in response to font changes.

Closes: #1301
Closes: #1435
Reported-by: Aaron Boxer @boxerab (github)
Reported-by: Josh Taylor <joshuataylorx@gmail.com>
Signed-off-by: David Aguilar <davvid@gmail.com>

cola/widgets/spellcheck.py
@@ -161,16 +161,8 @@ class SpellCheckLineEdit(SpellCheckTextEdit):
 
     def minimumSizeHint(self):
         """Match QLineEdit's size behavior"""
-        block_fmt = self.document().firstBlock().blockFormat()
         width = super().minimumSizeHint().width()
-        height = int(
-            QtGui.QFontMetricsF(self.font()).lineSpacing()
-            + block_fmt.topMargin()
-            + block_fmt.bottomMargin()
-            + self.document().documentMargin()
-            + 2 * self.frameWidth()
-        )
-
+        height = self._get_preferred_height()
         style_opts = QtWidgets.QStyleOptionFrame()
         style_opts.initFrom(self)
         style_opts.lineWidth = self.frameWidth()
@@ -182,6 +174,23 @@ class SpellCheckLineEdit(SpellCheckTextEdit):
     def sizeHint(self):
         """Use the minimum size as the sizeHint()"""
         return self.minimumSizeHint()
+
+    def setFont(self, font):
+        """Set the current font"""
+        super().setFont(font)
+        self.setMinimumHeight(self._get_preferred_height())
+
+    def _get_preferred_height(self):
+        """Calculate the preferred height for this widget"""
+        block_fmt = self.document().firstBlock().blockFormat()
+        height = int(
+            QtGui.QFontMetricsF(self.font()).lineSpacing()
+            + block_fmt.topMargin()
+            + block_fmt.bottomMargin()
+            + 2 * self.document().documentMargin()
+            + 2 * self.frameWidth()
+        )
+        return height
 
     def _trim_changed_text_lines(self):
         """Trim the document to a single line to enforce a maximum of one line"""

commit 37592a22f53d35e257ababa20e0f70e5d72ff2c0

spellcheck: tighten the minimum height when calculating the widget height
e7425622c3105109e0f489c4f849b24c1a11c1ed inadvertently adjusted the
document margin calculation. Use the original calculation.

Related-to: #1301 #1435
Signed-off-by: David Aguilar <davvid@gmail.com>

cola/widgets/spellcheck.py
@@ -187,7 +187,7 @@ class SpellCheckLineEdit(SpellCheckTextEdit):
             QtGui.QFontMetricsF(self.font()).lineSpacing()
             + block_fmt.topMargin()
             + block_fmt.bottomMargin()
-            + 2 * self.document().documentMargin()
+            + self.document().documentMargin()
             + 2 * self.frameWidth()
         )
         return height