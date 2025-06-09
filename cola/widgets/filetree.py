73cbe3457beab367dc45f53e069e83669a19cc86
David Aguilar <davvid@gmail.com>
2025-06-08 21:50:54 -0700

    filetree: add comments
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- cola/widgets/filetree.py
+++ cola/widgets/filetree.py
@@ -6,12 +6,15 @@ from . import standard
 
 
 class FileTree(standard.TreeWidget):
+    """A flag list of files presented using a tree widget"""
+
     def __init__(self, parent=None):
         standard.TreeWidget.__init__(self, parent=parent)
         self.setSelectionMode(self.ExtendedSelection)
         self.setHeaderHidden(True)
 
     def set_filenames(self, filenames, select=False):
+        """Update the tree to match the specified filenames"""
         self.clear()
         if not filenames:
             return
@@ -29,9 +32,11 @@ class FileTree(standard.TreeWidget):
             items[0].setSelected(True)
 
     def has_selection(self):
+        """Does the tree have an item currently selected?"""
         return bool(self.selectedItems())
 
     def selected_filenames(self):
+        """Return the currently selected filenames"""
         items = self.selectedItems()
         if not items:
             return []
@@ -39,4 +44,5 @@ class FileTree(standard.TreeWidget):
 
 
 def filename_from_item(item):
+    """Extract the filename user data from the QTreeWidgetItem"""
     return item.data(0, QtCore.Qt.UserRole)
