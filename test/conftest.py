9522db0d66a481fe465121200b54f18cb887f0cc
David Aguilar <davvid@gmail.com>
2026-08-03 23:58:39 -0700

    tests: handle offscreen configuration using conftest.py
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- test/conftest.py
+++ test/conftest.py
@@ -2,8 +2,8 @@
 
 import os
 
-
 # Use an offscreen Qt backend in CI and headless local sessions.
 # This prevents QApplication from aborting when the display server is unavailable
 # or the active platform plugin is unstable.
-os.environ.setdefault('QT_QPA_PLATFORM', 'offscreen')
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+os.environ['QT_QPA_PLATFORMTHEME'] = 'offscreen'
